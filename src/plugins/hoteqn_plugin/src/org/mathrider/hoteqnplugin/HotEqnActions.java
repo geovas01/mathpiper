10fdacd3a3e41d7a3b9fcb128595adabbce31a94
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-04-06 04:36:52 +0000

    Initial commit.

---
+++ src/plugins/hoteqn_plugin/src/org/mathrider/hoteqnplugin/HotEqnActions.java
@@ -0,0 +1,32 @@
+package org.mathrider.geogebraplugin;
+
+/*
+ * HotEqnActions.java
+ * part of the HotEqn plugin for the jEdit text editor
+ * Copyright (C) 2008 Ted Kosan
+
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id: HotEqnActions.java 5275 2005-09-10 19:40:17Z ezust $
+ */
+
+interface HotEqnActions {
+	void reset();
+
+	//void saveFile();
+
+	//void copyToBuffer();
+}
---
+++ src/plugins/hoteqn_plugin/src/org/mathrider/hoteqnplugin/HotEqnPlugin.java
@@ -0,0 +1,35 @@
+package org.mathrider.geogebraplugin;
+
+/*
+ * HotEqnPlugin.java
+ * part of the HotEqn plugin for the jEdit text editor
+ * Copyright (C) 2008 Ted Kosan.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id: HotEqnPlugin.java 5275 2005-09-10 19:40:17Z ezust $
+ */
+
+import org.gjt.sp.jedit.EditPlugin;
+
+/**
+ * The HotEqn plugin
+ * 
+ * @author Ted Kosan
+ */
+public class HotEqnPlugin extends EditPlugin {
+	public static final String NAME = "geogebra";
+	public static final String OPTION_PREFIX = "options.geogebra.";
+}
