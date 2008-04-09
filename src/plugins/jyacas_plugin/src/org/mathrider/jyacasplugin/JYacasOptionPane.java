38de33447d393ea64dbb51c896d54be74fa6a2a4
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-04-09 20:03:09 +0000

    Adding licensing information.

--- src/plugins/jyacas_plugin/src/org/mathrider/jyacasplugin/JYacasOptionPane.java
+++ src/plugins/jyacas_plugin/src/org/mathrider/jyacasplugin/JYacasOptionPane.java
@@ -1,27 +1,6 @@
+//Copyright (C) 2008 Ted Kosan (license information is at the end of this document.)
+
 package org.mathrider.jyacasplugin;
-
-/*
- * JYacasOptionPane.java
- * part of the JYacas plugin for the jEdit text editor
- * Copyright (C) 2008 Ted Kosan
-
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * $Id: JYacasOptionPane.java 5275 2005-09-10 19:40:17Z ezust $
- */
 
 import java.awt.BorderLayout;
 import java.awt.Font;
@@ -126,4 +105,21 @@ public class JYacasOptionPane extends AbstractOptionPane implements
 		return new Font(family, style, size);
 	}
 
-}
+}//
+
+
+/* {{{ License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ //}}}
--- src/plugins/jyacas_plugin/src/org/mathrider/jyacasplugin/JYacasToolPanel.java
+++ src/plugins/jyacas_plugin/src/org/mathrider/jyacasplugin/JYacasToolPanel.java
@@ -1,27 +1,6 @@
+//Copyright (C) 2008 Ted Kosan (license information is at the end of this document.)
+
 package org.mathrider.jyacasplugin;
-
-/*
- * JYacasToolPanel.java
- * part of the JYacas plugin for the jEdit text editor
- * Copyright (C) 2008 Ted Kosan
-
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * $Id: JYacasToolPanel.java 5275 2005-09-10 19:40:17Z ezust $
- */
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -99,4 +78,20 @@ public class JYacasToolPanel extends JPanel {
 		return b;
 	}
 
-}
+}//end class.
+
+/* {{{ License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ //}}}
