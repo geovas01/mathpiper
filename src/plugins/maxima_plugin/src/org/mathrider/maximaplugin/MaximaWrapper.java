65812bc12019eef7df7507ea1eee7622dd235953
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-05-31 04:59:39 +0000

    Added threading to maxima shell.

--- src/plugins/maxima_plugin/src/org/mathrider/maximaplugin/MaximaWrapper.java
+++ src/plugins/maxima_plugin/src/org/mathrider/maximaplugin/MaximaWrapper.java
@@ -3,6 +3,7 @@ package org.mathrider.maximaplugin;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.mathrider.ResponseListener;
 
 import java.io.*;
 
@@ -15,38 +16,47 @@ import errorlist.*;
  */
 
 
- public class MaximaWrapper { 
+ public class MaximaWrapper implements Runnable 
+ { 
 
 //	private static org.gjt.sp.jedit.bsh.Interpreter bshInstance;
 	private static MaximaWrapper maximaInstance = null;
 	
-	private StringBuffer resultBuffer;
+	private StringBuffer responseBuffer;
 	private Pattern inputPromptPattern;
 	private InputStream inputStream;
 	private OutputStream outputStream;
-	private String result;
+	private String response;
 	private String startMessage;
+	private ArrayList<ResponseListener> responseListeners;
+	private boolean keepRunning;
+	private String prompt;
 
     /** Creates a new instance of MaximaWrapper */
     protected MaximaWrapper() throws IOException
 	{
-
-	
+		responseListeners = new ArrayList<ResponseListener>();
 		ArrayList command = new ArrayList();
 		command.add("C:\\Program Files\\Maxima-5.15.0\\bin\\maxima.bat");
 		ProcessBuilder processBuilder = new ProcessBuilder(command);
 		Process maximaProcess = processBuilder.start();
 		inputStream = maximaProcess.getInputStream();
 		outputStream = maximaProcess.getOutputStream();
-		resultBuffer = new StringBuffer();
+		responseBuffer = new StringBuffer();
 		inputPromptPattern = Pattern.compile("\\n\\(%i[0-9]+\\)");
 		startMessage = getResponse();
+		new Thread(this,"maxima").start();
  
     }//end constructor.
 	
 	public String getStartMessage()
 	{
 		return startMessage;
+	}//end method.
+	
+	public String getPrompt()
+	{
+		return prompt;
 	}//end method.
 	
    public static MaximaWrapper getInstance() throws IOException
@@ -59,20 +69,46 @@ import errorlist.*;
    
    
    
-    String evaluate(String send) throws IOException
+    public synchronized void send(String send) throws IOException
     {
         outputStream.write(send.getBytes());
         outputStream.flush();
         //new Thread( max ).start();
-        return getResponse();
+        //return getResponse();
     }//end send.
-   
-   
-	public String getResponse() throws IOException
-    {
-	    boolean keepRunning = true;
+	
+	
+	public void run()
+	{
+		keepRunning = true;
 		
-		mainLoop: while(keepRunning)
+		String response;
+		
+		while(keepRunning == true)
+		{
+			try
+			{
+				response = getResponse();
+				notifyListeners(response);
+			}catch(IOException ioe)
+			{
+				notifyListeners(ioe.toString());
+			}
+		}//end while.
+		
+	}//end method.
+	
+	public void stop()
+	{
+		keepRunning = false;
+	}//end method.
+   
+   
+	protected String getResponse() throws IOException
+    {
+	    boolean keepChecking = true;
+		
+		mainLoop: while(keepChecking)
         {
              int serialAvailable = inputStream.available();
              if (serialAvailable == 0)
@@ -91,19 +127,22 @@ import errorlist.*;
                byte[] bytes = new byte[serialAvailable];
                 
 				inputStream.read( bytes, 0, serialAvailable );
-               resultBuffer.append(new String(bytes));
-               result = resultBuffer.toString();
+               responseBuffer.append(new String(bytes));
+               response = responseBuffer.toString();
                //System.out.println("SSSSS " + str);
-               Matcher matcher = inputPromptPattern.matcher(result);
+               Matcher matcher = inputPromptPattern.matcher(response);
                if(matcher.find())
                {
-                    //System.out.println("PPPPPP found end");
-                    resultBuffer.delete(0,resultBuffer.length());
-                    keepRunning = false;
+                  //System.out.println("PPPPPP found end");
+                  responseBuffer.delete(0,responseBuffer.length());
+				   int promptIndex = response.lastIndexOf("(%");
+				   prompt = response.substring(promptIndex,response.length());
+				  	response = response.substring(0,promptIndex);
+                   keepChecking = false;
                     
                }//end if.
 
-                System.out.println("NNNNNNN " + result);
+                //System.out.println("NNNNNNN " + response);
 
 				//self.terminalOutputStream.write( bytes )
 				
@@ -113,9 +152,24 @@ import errorlist.*;
 
         }//end while.
 		
-		return result;
+		return response;
         
     }//end method
+	
+	public void addResponseListener(ResponseListener listener)
+	{
+		responseListeners.add(listener);
+	}//end method.
+	
+	protected void notifyListeners(String response)
+	{
+		//java.util.Iterator listeners = responseListeners.iterator();
+		for(ResponseListener listener : responseListeners)
+		{
+			listener.response(response);
+		}//end for.
+		
+	}//end method.
 
 
 
