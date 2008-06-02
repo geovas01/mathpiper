54cf0351d8c6aae4c1b008fe97b2703ed3f19ac6
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-06-02 21:17:43 +0000

    Fixed GeoGebra JMenu bug.

--- src/jedit_core/unix_run.sh
+++ src/jedit_core/unix_run.sh
@@ -1,1 +1,1 @@
-java -Duser.home=. -Xms64M -Xmx192M -classpath ./jedit.jar:./jars/piper.jar:./jars/geogebra.jar:JmolApplet.jar:HotEqn.jar org.gjt.sp.jedit.jEdit -noserver -settings=./settings
+java -Duser.home=. -Xms64M -Xmx192M -classpath ./jedit.jar:./jars/piper.jar:./jars/geogebra.jar:./jars/AppleJavaExtensions.jar org.gjt.sp.jedit.jEdit -noserver -settings=./settings
