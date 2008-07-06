7ea4ed1dc650728880036f05407a1dbf7f03f36e
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-07-06 03:50:46 +0000

    Catch all.

--- trunk/src/plugins/piper_plugin/src/org/mathrider/piperplugin/PiperInterpreter.java
+++ trunk/src/plugins/piper_plugin/src/org/mathrider/piperplugin/PiperInterpreter.java
@@ -1,6 +1,8 @@
+//Copyright (C) 2008 Ted Kosan (license information is at the end of this document.)
+
 package org.mathrider.piperplugin;
 
-import org.mathrider.piper.*;
+import org.mathrider.piper.CPiper;
 import org.mathrider.piper.Piperexception;
 import console.Output;
 
@@ -15,9 +17,8 @@ import errorlist.*;
  */
 
 
- public class PiperInterpreter { // extends LispOutput {
+ public class PiperInterpreter { 
 
-//	private static org.gjt.sp.jedit.bsh.Interpreter bshInstance;
 	private static PiperInterpreter instance = null;
     private CPiper piper;
 	private StringOutput stringOutput;
@@ -47,24 +48,7 @@ import errorlist.*;
       return instance;
    }//end method.
    
-   
-   //This provides a bsh instance that can be shared by the whole application.
-   //Note:tk:look into finding a better place to put this.
-//   public static org.gjt.sp.jedit.bsh.Interpreter getBshInstance()
-//   {
-//      if(bshInstance == null) {
-//         	bshInstance = new org.gjt.sp.jedit.bsh.Interpreter();
-//		 	//bshInstance.eval("import geogebra.GeoGebraApplet;");
-//			//bshInstance.set("ggb",org.mathrider.geogebraplugin.Geogebra.getGeoGebraApplet());
-//			//bshInstance.set("jEdit",org.gjt.sp.jedit.jEdit);
-//			//bshInstance.set("returnError","none");
-//      }
-//      return bshInstance;
-//   }//end method.
-   
-   
 
-   
    
    public java.util.zip.ZipFile getScriptsZip()
    {
@@ -91,7 +75,7 @@ import errorlist.*;
         }
  
         // Pass the absolute path of piper.jar to Piper.
-        LispStandard.zipFile = scriptsZip;
+        org.mathrider.piper.LispStandard.zipFile = scriptsZip;
  
         return true;
     }
@@ -128,5 +112,23 @@ import errorlist.*;
 
     
 }//end class.
+
+
+/* {{{ License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ //}}}
+
 
 // :indentSize=4:lineSeparator=\n:noTabs=false:tabSize=4:folding=explicit:collapseFolds=1:
