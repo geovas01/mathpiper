16afd8b5625cd5d1a542ef86b075824cb2cf7751
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-08-04 02:16:19 +0000

    Added Note: before tk.

--- trunk/src/library_apps/piper/src/org/mathrider/piper/Piperexception.java
+++ trunk/src/library_apps/piper/src/org/mathrider/piper/Piperexception.java
@@ -1,7 +1,7 @@
 package org.mathrider.piper;
 
 
-public class Piperexception extends Exception //tk: made this class public.
+public class Piperexception extends Exception //Note:tk: made this class public so that clients can use this exception.
 {
   public Piperexception(String message)
   {
