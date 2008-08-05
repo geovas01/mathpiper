d7e00703eff43f222549016dd195f3aa252370be
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-08-05 00:14:17 +0000

    Initial commit.

---
+++ trunk/src/plugins/u6502_plugin/src/org/mathrider/u6502plugin/U6502Shell.java
@@ -0,0 +1,158 @@
+//Copyright (C) 2008 Ted Kosan (license information is at the end of this document.)
+
+package org.mathrider.u6502plugin;
+
+import java.awt.Color;
+//import java.io.File;
+//import java.util.Enumeration;
+//import java.util.Properties;
+//import java.util.Vector;
+import java.io.*;
+
+
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.EditPlugin;
+
+import console.Console;
+import console.ConsolePane;
+import console.Output;
+import console.Shell;
+
+
+public class U6502Shell extends Shell
+{
+	
+	
+	public U6502Shell()
+	{
+		super("U6502");
+		//interpreter = PiperInterpreter.getInstance(); //new StreamOutput(System.out) 
+		//Console console = (Console) jEdit.getPlugin("org.sageide.SAGEIDEPlugin").getPluginJAR().getClassLoader().loadClass("console.Console",1);
+		
+	}//end constructor.
+	
+	
+	
+	//{{{Begin Shell implementation.
+	public void printInfoMessage(Output output)
+	{
+		
+		//printUsage(jEdit.getProperty(PiperPlugin.NAME + ".shell.msg.info"), null, output);
+		//output.print(null, jEdit.getProperty(PiperPlugin.NAME + ".shell.msg.usage"));
+		
+		//try
+		//{
+			output.print(null, "U6502"); //"Piper version " + interpreter.evaluate("Version();") );
+		//}
+		//catch( org.mathrider.piper.Piperexception pe )
+		//{
+			output.print(java.awt.Color.RED,pe.getMessage() );
+		//}
+	
+	}//end method.
+
+	public void execute(Console console, String input, Output output, Output error, String command)
+	{
+		//try 
+		//{
+			//String result = interpreter.evaluate(command);
+			output.print(java.awt.Color.BLUE,"Out> " + result);
+			
+			
+		//}catch(org.mathrider.piper.Piperexception pe) 
+		//{
+			output.print(java.awt.Color.RED,pe.getMessage() );
+		//}
+		//finally
+		//{
+			output.commandDone();
+		//}
+
+	}//end method.
+
+	public void stop(Console console)
+	{
+
+	}
+
+	public boolean waitFor(Console console)
+	{
+
+		return true;
+	}//end method.
+	
+	
+	public void printPrompt(Console console, Output output)
+	{
+		output.writeAttrs(ConsolePane.colorAttributes(console.getPlainColor()), "\nIn> ");
+	}//end method.
+	
+	
+
+	// }}}End Shell implementation
+	
+	
+	
+	//{{{ Print usage methods.
+	private void printCurrentProjectInfo(Color color, Output output)
+	{
+		/*
+		if (_currentProject == null)
+			return;
+		output.print(null, jEdit.getProperty(AntFarmPlugin.NAME
+			+ ".shell.label.current-file"));
+		String projectName = _currentProject.getName() != null ? _currentProject.getName()
+			: "Untitled";
+
+		output.print(color, projectName + " (" + _currentBuildFile.getAbsolutePath()
+			+ ")\n");
+
+		output.print(null, jEdit.getProperty(AntFarmPlugin.NAME
+			+ ".shell.label.available-targets"));
+		String info = "";
+		Enumeration targets = _currentProject.getTargets().keys();
+		while (targets.hasMoreElements())
+		{
+			String target = (String) targets.nextElement();
+			info += target + printDefaultLabel(_currentProject, target) + "\t";
+		}
+		output.print(color, info); */
+
+	}
+
+	private String printDefaultLabel(String target)
+	{
+		//if (project.getDefaultTarget().equals(target))
+			//return " [default]";
+		return "";
+	}
+
+	private void printUsage(String additionalMessage, Color color, Output output)
+	{
+		//output.print(color, additionalMessage);
+		//output.print(null, jEdit.getProperty(AntFarmPlugin.NAME + ".shell.msg.usage"));
+	}
+
+	//}}}
+
+}//end class.
+
+
+/* {{{ License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ //}}}
+
+// :indentSize=4:lineSeparator=\n:noTabs=false:tabSize=4:folding=explicit:collapseFolds=1:
