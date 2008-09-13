57b01fe7b5f8288c4eefbe30e5043309cbedcc91
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-09-13 06:23:12 +0000

    Adding clojure to MathRider.

--- trunk/src/jedit_core/unix_run.sh
+++ trunk/src/jedit_core/unix_run.sh
@@ -1,1 +1,1 @@
-java -Duser.home=. -Xms64M -Xmx192M -classpath ./jedit.jar:./jars/piper.jar:./jars/piper_me.jar:./jars/geogebra.jar:./jars/AppleJavaExtensions.jar org.gjt.sp.jedit.jEdit -noserver -settings=./settings
+java -Duser.home=. -Xms64M -Xmx192M -classpath ./jedit.jar:./jars/piper.jar:./jars/piper_me.jar:./jars/geogebra.jar:./jars/AppleJavaExtensions.jar:./jars/clojure.jar org.gjt.sp.jedit.jEdit -noserver -settings=./settings
