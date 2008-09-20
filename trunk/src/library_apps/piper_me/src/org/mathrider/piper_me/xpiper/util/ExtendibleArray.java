80920bbb9e8c68e82499ed941286a113497bbc18
ogli72 <ogli72@613135f6-e55d-11de-9043-17b7bd24f792>
2008-09-20 19:52:04 +0000

    started replacement for missing java.Math in Java ME

--- trunk/src/library_apps/piper_me/src/org/mathrider/piper_me/xpiper/util/ExtendibleArray.java
+++ trunk/src/library_apps/piper_me/src/org/mathrider/piper_me/xpiper/util/ExtendibleArray.java
@@ -4,9 +4,7 @@ package org.mathrider.piper_me.xpiper.util;
  Copyright (c) Oliver Glier
  
  Warning: The licence of this file does not imply that the project
- of which it is part of has similar licence terms. In fact, this
- licence is very liberal and might be overruled by the project's
- own licence, for example the GPL.
+ of which it is part of has similar licence terms. 
  
  Redistribution and use in source and binary forms, with or without 
  modification, are permitted provided that the following conditions 
