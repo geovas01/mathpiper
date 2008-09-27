b686dac69f6b405e5b018d81d8d23e970c5d48c8
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-09-27 07:18:24 +0000

    Continuing to move all classes in Functions into their own class files.

---
+++ trunk/src/library_apps/piper_refactor/src/org/mathrider/piper/builtin/functions/If.java
@@ -0,0 +1,56 @@
+/* {{{ License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ //}}}
+
+// :indentSize=4:lineSeparator=\n:noTabs=false:tabSize=4:folding=explicit:collapseFolds=0:
+package org.mathrider.piper.builtin.functions;
+
+import org.mathrider.piper.builtin.BuiltinFunction;
+import org.mathrider.piper.lisp.Environment;
+import org.mathrider.piper.lisp.LispError;
+import org.mathrider.piper.lisp.Pointer;
+import org.mathrider.piper.lisp.Standard;
+
+/**
+ *
+ * @author 
+ */
+public class If extends BuiltinFunction
+{
+
+    public void eval(Environment aEnvironment, int aStackTop) throws Exception
+    {
+        int nrArguments = Standard.internalListLength(ARGUMENT(aEnvironment, aStackTop, 0));
+        LispError.CHK_CORE(aEnvironment, aStackTop, nrArguments == 3 || nrArguments == 4, LispError.KLispErrWrongNumberOfArgs);
+
+        Pointer predicate = new Pointer();
+        aEnvironment.iEvaluator.eval(aEnvironment, predicate, ARGUMENT(aEnvironment, aStackTop, 1));
+
+        if (Standard.isTrue(aEnvironment, predicate))
+        {
+            aEnvironment.iEvaluator.eval(aEnvironment, RESULT(aEnvironment, aStackTop), argument(ARGUMENT(aEnvironment, aStackTop, 0), 2));
+        } else
+        {
+            LispError.CHK_ARG_CORE(aEnvironment, aStackTop, Standard.isFalse(aEnvironment, predicate), 1);
+            if (nrArguments == 4)
+            {
+                aEnvironment.iEvaluator.eval(aEnvironment, RESULT(aEnvironment, aStackTop), argument(ARGUMENT(aEnvironment, aStackTop, 0), 3));
+            } else
+            {
+                Standard.internalFalse(aEnvironment, RESULT(aEnvironment, aStackTop));
+            }
+        }
+    }
+}
---
+++ trunk/src/library_apps/piper_refactor/src/org/mathrider/piper/builtin/functions/IsAtom.java
@@ -0,0 +1,40 @@
+/* {{{ License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ //}}}
+
+// :indentSize=4:lineSeparator=\n:noTabs=false:tabSize=4:folding=explicit:collapseFolds=0:
+
+package org.mathrider.piper.builtin.functions;
+
+import org.mathrider.piper.builtin.BuiltinFunction;
+import org.mathrider.piper.lisp.Environment;
+import org.mathrider.piper.lisp.Pointer;
+import org.mathrider.piper.lisp.Standard;
+
+/**
+ *
+ * @author 
+ */
+public class IsAtom extends BuiltinFunction
+{
+
+    public void eval(Environment aEnvironment, int aStackTop) throws Exception
+    {
+        Pointer result = new Pointer();
+        result.set(ARGUMENT(aEnvironment, aStackTop, 1).get());
+        String s = result.get().string();
+        Standard.internalBoolean(aEnvironment, RESULT(aEnvironment, aStackTop), s != null);
+    }
+}
