b482841f269e7f24d35bf973d579f011e134a5c5
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-09-27 23:20:36 +0000

    Removing unneeded commented sections of code.

--- trunk/src/plugins/maxima_plugin/src/org/mathrider/maximaplugin/MaximaShell.java
+++ trunk/src/plugins/maxima_plugin/src/org/mathrider/maximaplugin/MaximaShell.java
@@ -27,17 +27,17 @@ public class MaximaShell extends Shell implements org.mathrider.ResponseListener
 	private String maximaStartMessage;
 	private Output lastRequestOutput;
 	private Console lastRequestConsole;
-	//private Console console;
+
 	
 	
 	public MaximaShell() throws IOException
 	{
 		super("Maxima");
 		
-		maxima = MaximaWrapper.getInstance(); //new StreamOutput(System.out) 
+		maxima = MaximaWrapper.getInstance(); 
 		maximaStartMessage = maxima.getStartMessage();
 		maxima.addResponseListener(this);
-		//Console console = (Console) jEdit.getPlugin("org.sageide.SAGEIDEPlugin").getPluginJAR().getClassLoader().loadClass("console.Console",1);
+
 		
 	}//end constructor.
 	
@@ -47,10 +47,6 @@ public class MaximaShell extends Shell implements org.mathrider.ResponseListener
 	public void printInfoMessage(Output output)
 	{
 		output.print(null,maximaStartMessage);
-		//printUsage(jEdit.getProperty(MaximaPlugin.NAME + ".shell.msg.info"), null, output);
-		//output.print(null, jEdit.getProperty(MaximaPlugin.NAME + ".shell.msg.usage"));
-		
-
 	
 	}//end method.
 	
@@ -101,191 +97,16 @@ public class MaximaShell extends Shell implements org.mathrider.ResponseListener
 			output.commandDone();
 		}
 
-		/*
-		stop(console);
-		_output = output;
-		command = command.trim();
-
-		if (command.startsWith("!"))
-		{
-			if (_currentProject == null)
-			{
-
-				output.print(console.getErrorColor(),
-					jEdit.getProperty(AntFarmPlugin.NAME
-						+ ".shell.msg.non-selected"));
-				output.commandDone();
-				return;
-			}
-			_currentTarget = command.indexOf(" ") > 0 ? command.substring(1, command
-				.indexOf(" ")) : command.substring(1);
-
-			if (_currentTarget.equals(""))
-			{
-				_currentTarget = _currentProject.getDefaultTarget();
-			}
-			Target target = (Target) _currentProject.getTargets().get(_currentTarget);
-
-			if (target == null)
-			{
-				printUsage("Not a valid target: " + _currentTarget, console
-					.getErrorColor(), output);
-				output.commandDone();
-				return;
-			}
-
-			_targetRunner = new TargetRunner(target, _currentBuildFile, console
-				.getView(), output, AntCommandParser
-				.parseAntCommandProperties(command));
-		}
-		else if (command.equals("?"))
-		{
-
-			printUsage("", null, output);
-
-			Vector buildFiles = getAntFarm(console).getAntBuildFiles();
-			output.print(null, jEdit.getProperty(AntFarmPlugin.NAME
-				+ ".shell.label.available-files"));
-
-			String fileList = "";
-			for (int i = 0; i < buildFiles.size(); i++)
-			{
-				fileList += "(" + (i + 1) + ") " + buildFiles.elementAt(i) + "\n";
-			}
-			output.print(console.getInfoColor(), fileList);
-
-			printCurrentProjectInfo(console.getInfoColor(), output);
-
-			output.commandDone();
-		}
-		else if (command.startsWith("="))
-		{
-
-			Vector buildFiles = getAntFarm(console).getAntBuildFiles();
-
-			try
-			{
-				int fileNumber = Integer.parseInt(command.substring(1));
-
-				if (fileNumber - 1 >= buildFiles.size())
-				{
-					printUsage(jEdit.getProperty(AntFarmPlugin.NAME
-						+ ".shell.msg.no-build-file"), console
-						.getErrorColor(), output);
-					output.commandDone();
-					return;
-				}
-				String buildFilePath = (String) buildFiles
-					.elementAt(fileNumber - 1);
-				File buildFile = new File(buildFilePath);
-				Project project = null;
-				try
-				{
-					project = getAntFarm(console).getProject(
-						buildFile.getAbsolutePath());
-					setCurrentProject(project, buildFile, console);
-				}
-				catch (Exception e)
-				{
-					setCurrentProject(null, buildFile, console);
-					output.print(console.getErrorColor(), jEdit
-						.getProperty(AntFarmPlugin.NAME
-							+ ".shell.msg.broken-file")
-						+ buildFile + "\n" + e);
-					output.commandDone();
-					return;
-				}
-				output.commandDone();
-			}
-			catch (NumberFormatException e)
-			{
-				printUsage(jEdit.getProperty(AntFarmPlugin.NAME
-					+ ".shell.msg.invalid-usage"), console.getErrorColor(),
-					output);
-				output.commandDone();
-				return;
-			}
-		}
-		else if (command.startsWith("+"))
-		{
-
-			String filePath = command.substring(1);
-			try
-			{
-
-				// Get the provided path, or look up the tree.
-				File file = getFile(filePath, console);
-				filePath = FileUtils.getAbsolutePath(file);
-
-				// try to reload the project
-				getAntFarm(console).reloadAntBuildFile(filePath);
-
-				// initiate the project
-				Project project = getAntFarm(console).getProject(filePath);
-				setCurrentProject(project, file, console);
-				getAntFarm(console).addAntBuildFile(filePath);
-			}
-			catch (Exception e)
-			{
-				output.print(console.getErrorColor(), jEdit
-					.getProperty(AntFarmPlugin.NAME + ".shell.msg.broken-file")
-					+ filePath + "\n" + e);
-				output.commandDone();
-				return;
-			}
-			output.commandDone();
-		}
-		else if (!command.equals(""))
-		{
-			printUsage(jEdit.getProperty(AntFarmPlugin.NAME
-				+ ".shell.msg.invalid-usage"), console.getErrorColor(), output);
-		}
-		
-		*/
-		//output.commandDone();	
 	}//end method.
 
 	public void stop(Console console)
 	{
-		/*
-		if (_targetRunner != null)
-		{
-			if (_targetRunner.isAlive())
-			{
-				_targetRunner.stop();
 
-				if (_output != null)
-				{
-					_output.print(console.getErrorColor(), jEdit
-						.getProperty(AntFarmPlugin.NAME
-							+ ".shell.msg.killed"));
-				}
-			}
-			_targetRunner.resetLogging();
-			_targetRunner = null;
-		}
-		*/
 	}
 
 	public boolean waitFor(Console console)
 	{
-		/*
-		if (_targetRunner != null)
-		{
-			try
-			{
-				synchronized (_targetRunner)
-				{
-					_targetRunner.join();
-					_targetRunner = null;
-				}
-			}
-			catch (InterruptedException ie)
-			{
-				return false;
-			}
-		}
-		*/
+
 		return true;
 	}//end method.
 	
@@ -304,41 +125,18 @@ public class MaximaShell extends Shell implements org.mathrider.ResponseListener
 	//{{{ Print usage methods.
 	private void printCurrentProjectInfo(Color color, Output output)
 	{
-		/*
-		if (_currentProject == null)
-			return;
-		output.print(null, jEdit.getProperty(AntFarmPlugin.NAME
-			+ ".shell.label.current-file"));
-		String projectName = _currentProject.getName() != null ? _currentProject.getName()
-			: "Untitled";
-
-		output.print(color, projectName + " (" + _currentBuildFile.getAbsolutePath()
-			+ ")\n");
-
-		output.print(null, jEdit.getProperty(AntFarmPlugin.NAME
-			+ ".shell.label.available-targets"));
-		String info = "";
-		Enumeration targets = _currentProject.getTargets().keys();
-		while (targets.hasMoreElements())
-		{
-			String target = (String) targets.nextElement();
-			info += target + printDefaultLabel(_currentProject, target) + "\t";
-		}
-		output.print(color, info); */
 
 	}
 
 	private String printDefaultLabel(String target)
 	{
-		//if (project.getDefaultTarget().equals(target))
-			//return " [default]";
+
 		return "";
 	}
 
 	private void printUsage(String additionalMessage, Color color, Output output)
 	{
-		//output.print(color, additionalMessage);
-		//output.print(null, jEdit.getProperty(AntFarmPlugin.NAME + ".shell.msg.usage"));
+
 	}
 
 	//}}}
