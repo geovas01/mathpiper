0de6a4610971246ed9cd5e7f8b2b0535db747bfc
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-09-28 04:39:20 +0000

    Continuing to move classes out of Functions and into their own files.

---
+++ src/library_apps/piper_refactor/src/org/mathrider/piper/builtin/functions/FindFile.java
@@ -0,0 +1,51 @@
+/* {{{ License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ //}}}
+
+// :indentSize=4:lineSeparator=\n:noTabs=false:tabSize=4:folding=explicit:collapseFolds=0:
+
+package org.mathrider.piper.builtin.functions;
+
+import org.mathrider.piper.builtin.BuiltinFunction;
+import org.mathrider.piper.lisp.Atom;
+import org.mathrider.piper.lisp.Environment;
+import org.mathrider.piper.lisp.LispError;
+import org.mathrider.piper.lisp.Pointer;
+import org.mathrider.piper.lisp.Standard;
+
+/**
+ *
+ * @author 
+ */
+public class FindFile extends BuiltinFunction
+{
+
+    public void eval(Environment aEnvironment, int aStackTop) throws Exception
+    {
+        LispError.CHK_CORE(aEnvironment, aStackTop, aEnvironment.iSecure == false, LispError.KLispErrSecurityBreach);
+
+        Pointer evaluated = new Pointer();
+        evaluated.set(ARGUMENT(aEnvironment, aStackTop, 1).get());
+
+        // Get file name
+        LispError.CHK_ARG_CORE(aEnvironment, aStackTop, evaluated.get() != null, 1);
+        String orig = evaluated.get().string();
+        LispError.CHK_ARG_CORE(aEnvironment, aStackTop, orig != null, 1);
+        String oper = Standard.internalUnstringify(orig);
+
+        String filename = Standard.internalFindFile(oper, aEnvironment.iInputDirectories);
+        RESULT(aEnvironment, aStackTop).set(Atom.getInstance(aEnvironment, aEnvironment.hashTable().lookUpStringify(filename)));
+    }
+}
