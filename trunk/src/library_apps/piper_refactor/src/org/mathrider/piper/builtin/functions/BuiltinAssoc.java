e2722e0dae7f1e09c844d1a2bea112a334d67242
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-09-28 06:04:06 +0000

    Continuing to move classes out of Functions and into their own files.

---
+++ trunk/src/library_apps/piper_refactor/src/org/mathrider/piper/builtin/functions/BuiltinAssoc.java
@@ -0,0 +1,75 @@
+/* {{{ License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ //}}}
+
+// :indentSize=4:lineSeparator=\n:noTabs=false:tabSize=4:folding=explicit:collapseFolds=0:
+
+package org.mathrider.piper.builtin.functions;
+
+import org.mathrider.piper.builtin.BuiltinFunction;
+import org.mathrider.piper.lisp.Atom;
+import org.mathrider.piper.lisp.Cons;
+import org.mathrider.piper.lisp.Environment;
+import org.mathrider.piper.lisp.LispError;
+import org.mathrider.piper.lisp.Pointer;
+import org.mathrider.piper.lisp.Standard;
+
+/**
+ *
+ * @author 
+ */
+public class BuiltinAssoc extends BuiltinFunction
+{
+
+    public void eval(Environment aEnvironment, int aStackTop) throws Exception
+    {
+        // key to find
+        Pointer key = new Pointer();
+        key.set(ARGUMENT(aEnvironment, aStackTop, 1).get());
+
+        // assoc-list to find it in
+        Pointer list = new Pointer();
+        list.set(ARGUMENT(aEnvironment, aStackTop, 2).get());
+
+        Cons t;
+
+        //Check that it is a compound object
+        LispError.CHK_ARG_CORE(aEnvironment, aStackTop, list.get().subList() != null, 2);
+        t = list.get().subList().get();
+        LispError.CHK_ARG_CORE(aEnvironment, aStackTop, t != null, 2);
+        t = t.cdr().get();
+
+        while (t != null)
+        {
+            if (t.subList() != null)
+            {
+                Cons sub = t.subList().get();
+                if (sub != null)
+                {
+                    sub = sub.cdr().get();
+                    Pointer temp = new Pointer();
+                    temp.set(sub);
+                    if (Standard.internalEquals(aEnvironment, key, temp))
+                    {
+                        RESULT(aEnvironment, aStackTop).set(t);
+                        return;
+                    }
+                }
+            }
+            t = t.cdr().get();
+        }
+        RESULT(aEnvironment, aStackTop).set(Atom.getInstance(aEnvironment, "Empty"));
+    }
+}
---
+++ trunk/src/library_apps/piper_refactor/src/org/mathrider/piper/builtin/functions/ExplodeTag.java
@@ -0,0 +1,154 @@
+/* {{{ License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ //}}}
+
+// :indentSize=4:lineSeparator=\n:noTabs=false:tabSize=4:folding=explicit:collapseFolds=0:
+
+package org.mathrider.piper.builtin.functions;
+
+import org.mathrider.piper.builtin.BuiltinFunction;
+import org.mathrider.piper.lisp.Atom;
+import org.mathrider.piper.lisp.Cons;
+import org.mathrider.piper.lisp.Environment;
+import org.mathrider.piper.lisp.LispError;
+import org.mathrider.piper.lisp.Pointer;
+import org.mathrider.piper.lisp.SubList;
+import org.mathrider.piper.lisp.parsers.Tokenizer;
+
+/**
+ *
+ * @author 
+ */
+public class ExplodeTag extends BuiltinFunction
+{
+
+    public void eval(Environment aEnvironment, int aStackTop) throws Exception
+    {
+        Pointer out = new Pointer();
+        out.set(ARGUMENT(aEnvironment, aStackTop, 1).get());
+        LispError.CHK_ISSTRING_CORE(aEnvironment, aStackTop, out, 1);
+
+        String str = out.get().string();
+        int strInd = 0;
+        strInd++;
+        if (str.charAt(strInd) != '<')
+        {
+            RESULT(aEnvironment, aStackTop).set(out.get());
+            return;
+        }
+        LispError.CHK_ARG_CORE(aEnvironment, aStackTop, str.charAt(strInd) == '<', 1);
+        strInd++;
+        String type = "\"Open\"";
+
+        if (str.charAt(strInd) == '/')
+        {
+            type = "\"Close\"";
+            strInd++;
+        }
+        String tag = new String();
+
+        tag = tag + "\"";
+        while (Tokenizer.isAlpha(str.charAt(strInd)))
+        {
+            char c = str.charAt(strInd);
+            strInd++;
+            if (c >= 'a' && c <= 'z')
+            {
+                c = (char) (c + ('A' - 'a'));
+            }
+            tag = tag + c;
+        }
+        tag = tag + "\"";
+
+        Cons info = null;
+
+        while (str.charAt(strInd) == ' ')
+        {
+            strInd++;
+        }
+        while (str.charAt(strInd) != '>' && str.charAt(strInd) != '/')
+        {
+            String name = new String();
+            name = name + "\"";
+
+            while (Tokenizer.isAlpha(str.charAt(strInd)))
+            {
+                char c = str.charAt(strInd);
+                strInd++;
+                if (c >= 'a' && c <= 'z')
+                {
+                    c = (char) (c + ('A' - 'a'));
+                }
+                name = name + c;
+            }
+            name = name + "\"";
+            LispError.CHK_ARG_CORE(aEnvironment, aStackTop, str.charAt(strInd) == '=', 1);
+            strInd++;
+            LispError.CHK_ARG_CORE(aEnvironment, aStackTop, str.charAt(strInd) == '\"', 1);
+            String value = new String();
+
+            value = value + (str.charAt(strInd));
+            strInd++;
+            while (str.charAt(strInd) != '\"')
+            {
+                value = value + (str.charAt(strInd));
+                strInd++;
+            }
+            value = value + (str.charAt(strInd));
+            strInd++;
+
+            //printf("[%s], [%s]\n",name.String(),value.String());
+            {
+                Cons ls = Atom.getInstance(aEnvironment, "List");
+                Cons nm = Atom.getInstance(aEnvironment, name);
+                Cons vl = Atom.getInstance(aEnvironment, value);
+                nm.cdr().set(vl);
+                ls.cdr().set(nm);
+                Cons newinfo = SubList.getInstance(ls);
+                newinfo.cdr().set(info);
+                info = newinfo;
+            }
+            while (str.charAt(strInd) == ' ')
+            {
+                strInd++;
+
+            //printf("End is %c\n",str[0]);
+            }
+        }
+        if (str.charAt(strInd) == '/')
+        {
+            type = "\"OpenClose\"";
+            strInd++;
+            while (str.charAt(strInd) == ' ')
+            {
+                strInd++;
+            }
+        }
+        {
+            Cons ls = Atom.getInstance(aEnvironment, "List");
+            ls.cdr().set(info);
+            info = SubList.getInstance(ls);
+        }
+
+        Cons xm = Atom.getInstance(aEnvironment, "XmlTag");
+        Cons tg = Atom.getInstance(aEnvironment, tag);
+        Cons tp = Atom.getInstance(aEnvironment, type);
+        info.cdr().set(tp);
+        tg.cdr().set(info);
+        xm.cdr().set(tg);
+        RESULT(aEnvironment, aStackTop).set(SubList.getInstance(xm));
+
+    }
+}
