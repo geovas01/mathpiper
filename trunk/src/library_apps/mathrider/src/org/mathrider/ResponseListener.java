dd980f8df404c64daf79df78ff841d2f1cd9dd8d
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-11-16 21:53:13 +0000

    Added the capability to display the line number of a MathPiper error and to also obtain it as an integer.  MathPiper errors are also now placed into the Errorlist plugin.

--- trunk/src/library_apps/mathrider/src/org/mathrider/ResponseListener.java
+++ trunk/src/library_apps/mathrider/src/org/mathrider/ResponseListener.java
@@ -3,6 +3,6 @@ package org.mathrider;
 public interface ResponseListener
 {
     void response(String response);
-    void response(String[] response);
+    void response(Object[] response);
     boolean remove();
 }// end interface.
