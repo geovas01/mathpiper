c509b790e8c5f1b842e2bcea8d67da4c31ce4b57
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-12-14 03:28:37 +0000

    I now have a simple headless applet working which can be used to test JavaScript calls into MathPiper.

---
+++ src/library_apps/mathpiper/src/org/mathpiper/ui/gui/calculator/JSApplet.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.mathpiper.ui.gui.calculator;
+
+import javax.swing.JApplet;
+import org.mathpiper.interpreters.EvaluationResponse;
+import org.mathpiper.interpreters.Interpreter;
+import org.mathpiper.interpreters.Interpreters;
+
+/**
+ *
+ * @author tkosan
+ */
+public class JSApplet extends JApplet {
+
+private Interpreter mathPiper;
+
+   public void init(){
+
+
+     String docBase = getDocumentBase().toString();
+
+
+
+     mathPiper = Interpreters.newSynchronousInterpreter(docBase);
+
+
+   }
+
+
+public String evaluate(String expression)
+{
+    EvaluationResponse evaluationResponse;
+
+    evaluationResponse = mathPiper.evaluate(expression + ";");
+
+    String response = null;
+
+    if(evaluationResponse.isExceptionThrown())
+    {
+        response = evaluationResponse.getExceptionMessage();
+    }
+    else
+    {
+        response = evaluationResponse.getResult();
+    }
+
+    return response;
+}//end method.
+
+  public void start(){
+     System.out.println("Applet starting.");
+
+  }
+
+  public void stop(){
+     System.out.println("Applet stopping.");
+  }
+
+  public void destroy(){
+     System.out.println("Destroy method called.");
+  }
+
+
+
+
+}
