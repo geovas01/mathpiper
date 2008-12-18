eb6ae88ae5ea45c368c193dad48fc672a65ccf7c
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2008-12-18 02:05:45 +0000

    I am beginning to add javadocs to this class.

--- src/library_apps/mathpiper/src/org/mathpiper/Interfaces.java
+++ src/library_apps/mathpiper/src/org/mathpiper/Interfaces.java
@@ -11,70 +11,71 @@ import org.mathpiper.ui.gui.calculator.Calculator;
 import org.mathpiper.ui.gui.calculator.CalculatorPanel;
 
 /**
- *
+ * Primary class for obtaining various interfaces to MathPiper.
  */
-public class Interfaces
-{
+public class Interfaces {
 
-    public static void calculatorFrame()
-    {
-             java.awt.EventQueue.invokeLater(new Runnable() {
+    /**
+     * Shows a JFrame which contains a MathPiper calculator GUI.
+     */
+    public static void showCalculatorFrame() {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
             public void run() {
                 new Calculator().setVisible(true);
             }
         });
     }//end method.
 
-
-    public static JPanel newCalculatorPanel()
-    {
-         return new CalculatorPanel();
+    /**
+     * Returns a JPanel which contains the a MathPiper calculator GUI.
+     * @return a JPanel which contains the calculator.
+     */
+    public static JPanel newCalculatorPanel() {
+        return new CalculatorPanel();
     }//end method.
 
-    public static Interpreter newSynchronousInterpreter()
-    {
+    /**
+     * Creates a new synchronous MathPiper interpreter instance which access the script files using the classpath.
+     * @return
+     */
+    public static Interpreter newSynchronousInterpreter() {
         return Interpreters.newSynchronousInterpreter();
     }
 
-    public static Interpreter newSynchronousInterpreter(String docBase)
-    {
+    /**
+     * Creates a new synchronous MathPiper interpreter instance which access the script files using the a passed-in path.
+     * @return
+     */
+    public static Interpreter newSynchronousInterpreter(String docBase) {
         return Interpreters.newSynchronousInterpreter(docBase);
     }
 
-    public static Interpreter getSynchronousInterpreter()
-    {
+    public static Interpreter getSynchronousInterpreter() {
         return Interpreters.getSynchronousInterpreter();
     }
 
-     public static Interpreter getSynchronousInterpreter(String docBase)
-    {
+    public static Interpreter getSynchronousInterpreter(String docBase) {
         return Interpreters.getSynchronousInterpreter(docBase);
     }
 
-
-    public static Interpreter newAsynchronousInterpreter()
-    {
+    public static Interpreter newAsynchronousInterpreter() {
         return Interpreters.newAsynchronousInterpreter();
     }
 
-     public static Interpreter newAsynchronousInterpreter(String docBase)
-    {
+    public static Interpreter newAsynchronousInterpreter(String docBase) {
         return Interpreters.newAsynchronousInterpreter(docBase);
     }
 
-     public static Interpreter getAsynchronousInterpreter()
-    {
+    public static Interpreter getAsynchronousInterpreter() {
         return Interpreters.getAsynchronousInterpreter();
     }
 
-     public static Interpreter getAsynchronousInterpreter(String docBase)
-    {
+    public static Interpreter getAsynchronousInterpreter(String docBase) {
         return Interpreters.getAsynchronousInterpreter(docBase);
     }
-    
-    
-    public static void main(String[] args)
-    {
-        calculatorFrame();
+
+    public static void main(String[] args) {
+        showCalculatorFrame();
     }
 }
