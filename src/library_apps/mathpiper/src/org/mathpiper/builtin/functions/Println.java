cf2cb8af2eea4f1f501982fcf81e716376071c13
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2009-01-20 07:21:08 +0000

    Adding Println() function.

---
+++ src/library_apps/mathpiper/src/org/mathpiper/builtin/functions/Println.java
@@ -0,0 +1,44 @@
+/* {{{ License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ //}}}
+
+// :indentSize=4:lineSeparator=\n:noTabs=false:tabSize=4:folding=explicit:collapseFolds=0:
+
+package org.mathpiper.builtin.functions;
+
+import org.mathpiper.builtin.BuiltinFunctionInitialize;
+import org.mathpiper.lisp.Environment;
+import org.mathpiper.lisp.LispError;
+import org.mathpiper.lisp.UtilityFunctions;
+import org.mathpiper.lisp.userfunctions.UserFunction;
+
+/**
+ *
+ *
+ */
+public class Println extends BuiltinFunctionInitialize
+{
+
+    public void eval(Environment aEnvironment, int aStackTop) throws Exception
+    {
+        LispError.checkArgumentCore(aEnvironment, aStackTop, getArgumentPointer(aEnvironment, aStackTop, 1).getCons() != null, 1);
+        String orig = getArgumentPointer(aEnvironment, aStackTop, 1).getCons().string();
+        System.out.println(orig);
+        LispError.checkArgumentCore(aEnvironment, aStackTop, orig != null, 1);
+        String oper = UtilityFunctions.internalUnstringify(orig);
+//        UserFunction.traceOn();
+       // System.out.println(oper);
+    }
+}
