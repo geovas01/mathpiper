48e806e0ff60a93a3d4c9614633674b4fd3b65a9
ogli72 <ogli72@613135f6-e55d-11de-9043-17b7bd24f792>
2009-01-27 19:27:59 +0000

    Bogus unit test for piper-me hashmap

---
+++ src/library_apps/piper_me/test/org/eninom/collection/HashMapTest.java
@@ -0,0 +1,9 @@
+package org.eninom.collection;
+
+import junit.framework.TestCase;
+
+public class HashMapTest extends TestCase {
+  static public void testMap() {
+//does nothing, real unit tests will follow...
+  }
+}
