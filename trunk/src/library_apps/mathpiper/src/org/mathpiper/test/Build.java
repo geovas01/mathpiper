39848d1c4bc101392369c15e990e4ac27bcf38e6
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2009-02-25 08:20:37 +0000

    Starting to populate .mpi and .mpi.def files in .rep directories.

--- trunk/src/library_apps/mathpiper/src/org/mathpiper/test/Build.java
+++ trunk/src/library_apps/mathpiper/src/org/mathpiper/test/Build.java
@@ -19,14 +19,18 @@ package org.mathpiper.test;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -70,7 +74,7 @@ public class Build {
 
                 String output;
                 for (int x = 0; x < packagesDirectory.length; x++) {
-                    //Process each package directory.
+                    //Process each package directory.************************************************************************
                     File packageDirectoryFile = packagesDirectory[x];
                     String packageDirectoryFileName = packageDirectoryFile.getName();
                     System.out.println(packageDirectoryFileName);
@@ -81,6 +85,18 @@ public class Build {
                     String newPackagePath = outputDirectory + newPackageName;
                     File newPackageFile = new File(newPackagePath);
                     Boolean directoryCreated = newPackageFile.mkdir();
+
+                    //mpi file.
+                    BufferedWriter mpiFileOut = null;
+                    File newMPIFile = new File(newPackagePath + "/" + "code.mpi");
+                    newMPIFile.createNewFile();
+                    mpiFileOut = new BufferedWriter(new FileWriter(newMPIFile));
+
+                    //mpi.def file
+                    BufferedWriter mpiDefFileOut = null;
+                    File newMPIDefFile = new File(newPackagePath + "/" + "code.mpi.def");
+                    newMPIDefFile.createNewFile();
+                    mpiDefFileOut = new BufferedWriter(new FileWriter(newMPIDefFile));
 
 
                     //Place class files in package dir
@@ -99,17 +115,44 @@ public class Build {
                         Arrays.sort(packageDirectoryContentsArray);
 
 
-                        StringBuilder scriptClassBuffer = new StringBuilder();
                         // }//note:tk:remove.
                         String classNameUpper = null;
 
                         for (int x2 = 0; x2 < packageDirectoryContentsArray.length; x2++) {
-                            scriptClassBuffer.delete(0, scriptClassBuffer.length());
                             //Process each script in a .rep directory.
                             File scriptFile = packageDirectoryContentsArray[x2];
                             System.out.println("    " + scriptFile.getName());
 
-                            if (!scriptFile.getName().endsWith(".mrw")) {
+                            if (scriptFile.getName().endsWith(".mrw")) {
+
+
+                                List<Fold> folds = scanMRWFile(scriptFile);
+
+                                for (Fold fold : folds) {
+
+                                    String foldType = fold.getType();
+                                    if (foldType.equalsIgnoreCase("%mathpiper")) {
+
+                                        mpiFileOut.write(fold.getContents());
+
+
+                                        if (fold.getAttributes().containsKey("def")) {
+                                            String defAttribute = (String) fold.getAttributes().get("def");
+                                            if (defAttribute.equalsIgnoreCase("true")) {
+                                                mpiDefFileOut.write(scriptFile.getName().replace(".mrw", ""));
+                                                mpiDefFileOut.write("\n");
+                                            }
+                                        }
+
+                                    } else if (foldType.equalsIgnoreCase("%html")) {
+                                    }
+
+
+
+
+                                }//end subpackage for.
+
+                            } else {
                                 java.io.File[] packageSubDirectoryContentsArray = scriptFile.listFiles(new java.io.FilenameFilter() {
 
                                     public boolean accept(java.io.File file, String name) {
@@ -126,25 +169,28 @@ public class Build {
 
 
                                 for (int x3 = 0; x3 < packageSubDirectoryContentsArray.length; x3++) {
-
                                     //Process each script in a .rep directory.
                                     File scriptFile2 = packageSubDirectoryContentsArray[x3];
-                                    System.out.println("        "+ scriptFile2.getName());
+                                    System.out.println("        " + scriptFile2.getName());
+                                }
+                            }//end else.
 
-                                }//end subpackage for.
-
-                            }//end subpackage if.
-
-                        }//end for.
+                        }//end package for.
 
 
 
 
                     }//end if. 
 
-
+                    if (mpiFileOut != null) {
+                        mpiFileOut.close();
+                        mpiDefFileOut.write("}\n");
+                        mpiDefFileOut.close();
+                    }
 
                 }//end for.
+
+
 
             } else {
                 System.out.println("\nDirectory " + scriptsDirectory + "does not exist.\n");
@@ -169,6 +215,9 @@ public class Build {
         }
 
         String outputDirectory = "/home/tkosan/NetBeansProjects/scripts/";
+        File newScriptsDirectory = new File(outputDirectory);
+        Boolean directoryCreated = newScriptsDirectory.mkdir();
+
         //String outputDirectory = "/home/tkosan/temp/mathpiper/org/mathpiper/scripts/";
 
         Build scripts = new Build();
@@ -211,7 +260,87 @@ public class Build {
         }
     }//end method.
 
+    List scanMRWFile(File mrwFile) {
+        List<Fold> folds = new ArrayList();
+        StringBuilder foldContents = new StringBuilder();
+        String foldHeader = "";
+        boolean inFold = false;
 
+        try {
+            // Open the file that is the first
+            // command line parameter
+            FileInputStream fstream = new FileInputStream(mrwFile);
+            // Get the object of DataInputStream
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line;
+            //Read File Line By Line
+            while ((line = br.readLine()) != null) {
+
+                //System.out.println(line);
+
+                if (line.startsWith("%/")) {
+                    Fold fold = new Fold(foldHeader, foldContents.toString());
+                    foldContents.delete(0, foldContents.length());
+                    folds.add(fold);
+
+                } else if (line.startsWith("%")) {
+                    foldHeader = line;
+                    inFold = true;
+                } else if (inFold == true) {
+                    foldContents.append(line);
+                    foldContents.append("\n");
+
+                }
+            }//end while.
+
+
+            //Close the input stream
+            in.close();
+
+        } catch (Exception e) {//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
+        return folds;
+
+    }//end.
+
+    class Fold {
+
+        private String type;
+        private String contents;
+        private Map<String, String> attributes = new HashMap();
+
+        public Fold(String header, String contents) {
+            scanHeader(header);
+
+            this.contents = contents;
+        }//end inner class.
+
+        private void scanHeader(String header) {
+            String[] headerParts = header.trim().split(",");
+
+            type = headerParts[0];
+
+            for (int x = 1; x < headerParts.length; x++) {
+                String[] headerPart = headerParts[x].split("=");
+                String attributeName = headerPart[0];
+                String attributeValue = headerPart[1].replace("\"", "");
+                attributes.put(attributeName, attributeValue);
+            }
+
+        }//end method.
+
+        public Map getAttributes() {
+            return attributes;
+        }
+
+        public String getContents() {
+            return contents;
+        }
+
+        public String getType() {
+            return type;
+        }
+    }//end inner class.
 }//end class.
-
-
