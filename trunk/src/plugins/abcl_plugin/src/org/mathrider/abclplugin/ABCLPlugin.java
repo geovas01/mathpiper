94a5d13722d236cc301318e4ecfc73b62f0009f3
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2009-03-11 07:47:01 +0000

    Beginning to add armedbear common lisp plugin to MathRider.

---
+++ trunk/src/plugins/abcl_plugin/src/org/mathrider/abclplugin/ABCLPlugin.java
@@ -0,0 +1,29 @@
+//Copyright (C) 2008 Ted Kosan (license information is at the end of this document.)
+package org.mathrider.abclplugin;
+
+import org.gjt.sp.jedit.EditPlugin;
+
+/**
+ * 
+ * @author Ted Kosan
+ */
+public class ABCLPlugin extends EditPlugin {
+	public static final String NAME = "abcl";
+	public static final String OPTION_PREFIX = "options.abcl.";
+}//end class.
+
+/* {{{ License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ //}}}
