0f2a1e8160088c64f523eff389eabea95efae0f2
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2009-05-02 05:21:57 +0000

    Removed an unneeded import.

--- trunk/src/library_apps/mathpiper/src/org/mathpiper/test/Build.java
+++ trunk/src/library_apps/mathpiper/src/org/mathpiper/test/Build.java
@@ -22,7 +22,6 @@ import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
