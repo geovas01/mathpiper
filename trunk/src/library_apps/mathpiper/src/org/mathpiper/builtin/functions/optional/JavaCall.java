93ff7dcf5283282f1d6045be77f852024013a597
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2009-05-23 08:56:52 +0000

    Catch all.

--- trunk/src/library_apps/mathpiper/src/org/mathpiper/builtin/functions/optional/JavaCall.java
+++ trunk/src/library_apps/mathpiper/src/org/mathpiper/builtin/functions/optional/JavaCall.java
@@ -33,63 +33,69 @@ import org.mathpiper.lisp.cons.ConsTraverser;
  */
 public class JavaCall extends BuiltinFunction {
 
-    //private StandardFileOutputStream out = new StandardFileOutputStream(System.out);
-    public void evaluate(Environment aEnvironment, int aStackTop) throws Exception {
-        ConsPointer subList = getArgumentPointer(aEnvironment, aStackTop, 1).getCons().getSublistPointer();
-        if (subList != null) {
-            ConsTraverser consTraverser = new ConsTraverser(subList);
+	//private StandardFileOutputStream out = new StandardFileOutputStream(System.out);
+	public void evaluate(Environment aEnvironment, int aStackTop) throws Exception {
+		ConsPointer subList = getArgumentPointer(aEnvironment, aStackTop, 1).getCons().getSublistPointer();
+		if (subList != null) {
+			ConsTraverser consTraverser = new ConsTraverser(subList);
 
-            //Skip past List type.
-            consTraverser.goNext();
+			//Skip past List type.
+			consTraverser.goNext();
 
-            Cons argumentCons;
+			Cons argumentCons;
 
-            //Obtain the name of the variable that the desired Java object is bound to.
-            argumentCons = consTraverser.getPointer().getCons();
-            ConsPointer result = new ConsPointer();
+			//Obtain the name of the variable that the desired Java object is bound to.
+			argumentCons = consTraverser.getPointer().getCons();
+			ConsPointer result = new ConsPointer();
 
-            String argumentConsString = argumentCons.string().replace("\"", "");
-            // argumentConsString = argumentConsString.replace("\"", "");
-            aEnvironment.getGlobalVariable(argumentConsString, result);
+			String argumentConsString = argumentCons.string().replace("\"", "");
+			// argumentConsString = argumentConsString.replace("\"", "");
+			aEnvironment.getGlobalVariable(argumentConsString, result);
 
-            BuiltinContainer builtinContainer;
+			BuiltinContainer builtinContainer;
 
-            if (result.getCons() != null) {
+			if (result.getCons() != null) {
 
-                builtinContainer = result.getCons().getGeneric();
+				builtinContainer = result.getCons().getGeneric();
 
-                if (builtinContainer != null) {
+				if (builtinContainer != null) {
 
-                    //System.out.println(argumentCons);
-                    consTraverser.goNext();
+					//System.out.println(argumentCons);
+					consTraverser.goNext();
 
-                    ArrayList argumentArrayList = new ArrayList();
-                    while (consTraverser.getCons() != null) {
-                        argumentCons = consTraverser.getPointer().getCons();
+					ArrayList argumentArrayList = new ArrayList();
+					while (consTraverser.getCons() != null) {
+						argumentCons = consTraverser.getPointer().getCons();
 
-                        String argumentString = argumentCons.string();
+						String argumentString = argumentCons.string();
 
-                        argumentString = argumentString.replace("\"", "");
-                        
-                        argumentArrayList.add(argumentString);
+						argumentString = argumentString.replace("\"", "");
 
-                        consTraverser.goNext();
+						argumentArrayList.add(argumentString);
 
-                    }//end while.
+						consTraverser.goNext();
+
+					}//end while.
 
 
-                    String response = builtinContainer.execute((String[]) argumentArrayList.toArray(new String[0]));
+					String response = builtinContainer.execute((String[]) argumentArrayList.toArray(new String[0]));
+					//System.out.println("XXXXXXXXXXX: " + response);
 
-                     getResult(aEnvironment, aStackTop).setCons(AtomCons.getInstance(aEnvironment, response));
-                     return;
+					if(response == null)
+					{
+						response = "null";
+					}
+					getResult(aEnvironment, aStackTop).setCons(AtomCons.getInstance(aEnvironment, response));
 
-                }//end if.
+					return;
 
-            }//end if.
+				}//end if.
 
-        }//end if.
+			}//end if.
 
-        UtilityFunctions.internalFalse(aEnvironment, getResult(aEnvironment, aStackTop));
+		}//end if.
 
-    }//end method.
+		UtilityFunctions.internalFalse(aEnvironment, getResult(aEnvironment, aStackTop));
+
+	}//end method.
 }
