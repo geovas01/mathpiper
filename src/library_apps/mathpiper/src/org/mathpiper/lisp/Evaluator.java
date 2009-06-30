a935c4ade8d6f58bb91229ab871ff545bb2ed256
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2009-06-30 17:35:40 +0000

    Fixed bug where function trace was not being indented properly.

--- src/library_apps/mathpiper/src/org/mathpiper/lisp/Evaluator.java
+++ src/library_apps/mathpiper/src/org/mathpiper/lisp/Evaluator.java
@@ -32,257 +32,266 @@ import org.mathpiper.lisp.stacks.UserStackInformation;
 // evaluate'
 public abstract class Evaluator {
 
-    public static boolean DEBUG = false;
-    public static boolean TRACE_TO_STANDARD_OUT = false;
-    public static boolean VERBOSE_DEBUG = false;
-    private static int evalDepth = 0;
-    public static boolean iTraced = false;
-    private static List traceFunctionList = null;
-    private static List traceExceptFunctionList = null;
-    UserStackInformation iBasicInfo = new UserStackInformation();
+	public static boolean DEBUG = false;
+	public static boolean TRACE_TO_STANDARD_OUT = false;
+	public static boolean VERBOSE_DEBUG = false;
+	private static int evalDepth = 0;
+	public static boolean iTraced = false;
+	private static List traceFunctionList = null;
+	private static List traceExceptFunctionList = null;
+	UserStackInformation iBasicInfo = new UserStackInformation();
 
-    public static void showExpression(StringBuffer outString, Environment aEnvironment, ConsPointer aExpression) throws Exception {
-        MathPiperPrinter infixprinter = new MathPiperPrinter(aEnvironment.iPrefixOperators, aEnvironment.iInfixOperators, aEnvironment.iPostfixOperators, aEnvironment.iBodiedOperators);
-        // Print out the current expression
-        //StringOutput stream(outString);
-        MathPiperOutputStream stream = new StringOutputStream(outString);
-        infixprinter.print(aExpression, stream, aEnvironment);
-        // Escape quotes.
-        for (int i = outString.length() - 1; i >= 0; --i) {
-            char c = outString.charAt(i);
-            if (c == '\"') {
-                //outString.insert(i, '\\');
-                outString.deleteCharAt(i);
-            }
-        }// end for.
+	public static void showExpression(StringBuffer outString, Environment aEnvironment, ConsPointer aExpression) throws Exception {
+		MathPiperPrinter infixprinter = new MathPiperPrinter(aEnvironment.iPrefixOperators, aEnvironment.iInfixOperators, aEnvironment.iPostfixOperators, aEnvironment.iBodiedOperators);
+		// Print out the current expression
+		//StringOutput stream(outString);
+		MathPiperOutputStream stream = new StringOutputStream(outString);
+		infixprinter.print(aExpression, stream, aEnvironment);
+		// Escape quotes.
+		for (int i = outString.length() - 1; i >= 0; --i) {
+			char c = outString.charAt(i);
+			if (c == '\"') {
+				//outString.insert(i, '\\');
+				outString.deleteCharAt(i);
+			}
+		}// end for.
 
-    }//end method.
+	}//end method.
 
-    public static void traceShowEnter(Environment aEnvironment, ConsPointer aExpression, String extraInfo) throws Exception {
-        for (int i = 0; i < evalDepth; i++) {
-            // aEnvironment.iEvalDepth; i++) {
-            if (TRACE_TO_STANDARD_OUT) {
-                System.out.print("    ");
-            } else {
-                aEnvironment.write("    ");
-            }
-        }
-        if (TRACE_TO_STANDARD_OUT) {
-            System.out.print("Enter<" + extraInfo + ">{(");
-        } else {
-            aEnvironment.write("Enter<" + extraInfo + ">{(");
-        }
+	public static void traceShowEnter(Environment aEnvironment, ConsPointer aExpression, String extraInfo) throws Exception {
+		for (int i = 0; i < evalDepth; i++) {
+			// aEnvironment.iEvalDepth; i++) {
+			if (TRACE_TO_STANDARD_OUT) {
+				System.out.print("    ");
+			} else {
+				aEnvironment.write("    ");
+			}
+		}//end for.
 
-        String function = "";
-        if (aExpression.getCons().getSublistPointer() != null) {
-            ConsPointer sub = aExpression.getCons().getSublistPointer();
-            if (sub.getCons().string() != null) {
-                function = sub.getCons().string();
-            }
-        }
-        if (TRACE_TO_STANDARD_OUT) {
-            System.out.print(function);
-        } else {
-            aEnvironment.write(function);
-        }//end else.
+		if (TRACE_TO_STANDARD_OUT) {
+			System.out.print("Enter<" + extraInfo + ">{(");
+		} else {
+			aEnvironment.write("Enter<" + extraInfo + ">{(");
+		}
 
-        if (TRACE_TO_STANDARD_OUT) {
-            System.out.print(", ");
-        } else {
-            aEnvironment.write(", ");
-        }
-        traceShowExpression(aEnvironment, aExpression);
+		String function = "";
+		if (aExpression.getCons().getSublistPointer() != null) {
+			ConsPointer sub = aExpression.getCons().getSublistPointer();
+			if (sub.getCons().string() != null) {
+				function = sub.getCons().string();
+			}
+		}
+		if (TRACE_TO_STANDARD_OUT) {
+			System.out.print(function);
+		} else {
+			aEnvironment.write(function);
+		}//end else.
 
-        if (TRACE_TO_STANDARD_OUT) {
-            System.out.print(");\n");
-        } else {
-            aEnvironment.write(");\n");
-        }
+		if (TRACE_TO_STANDARD_OUT) {
+			System.out.print(", ");
+		} else {
+			aEnvironment.write(", ");
+		}
+		traceShowExpression(aEnvironment, aExpression);
 
-        if (traceFunctionList != null) {
-            //Trace stack frames.
-            List<String> functionsOnStack = new ArrayList();
-            LocalVariableFrame localVariableFrame = aEnvironment.iLocalVariablesFrame;
-            while (localVariableFrame != null) {
-                functionsOnStack.add(localVariableFrame.getFunctionName());
-                localVariableFrame = localVariableFrame.iNext;
-            }//end while
-            Collections.reverse(functionsOnStack);
-            StringBuilder functionsDump = new StringBuilder();
-            functionsDump.append("(User Function Call Stack: ");
-            for(String functionName: functionsOnStack)
-            {
-                functionsDump.append(functionName + ", ");
-            }//end for.
-            functionsDump.append(")\n");
-            if (TRACE_TO_STANDARD_OUT) {
-                System.out.print(functionsDump.toString());
-            } else {
-                aEnvironment.write(functionsDump.toString());
-            }//end else.
-        }//end if.
+		if (TRACE_TO_STANDARD_OUT) {
+			System.out.print(");\n");
+		} else {
+			aEnvironment.write(");\n");
+		}
+
+		if (traceFunctionList != null) {
+			for (int i = 0; i < (evalDepth + 1); i++) {
+				if (TRACE_TO_STANDARD_OUT) {
+					System.out.print("    ");
+				} else {
+					aEnvironment.write("    ");
+				}
+			}//end for.
+
+			//Trace stack frames.
+			List<String> functionsOnStack = new ArrayList();
+			LocalVariableFrame localVariableFrame = aEnvironment.iLocalVariablesFrame;
+			while (localVariableFrame != null) {
+				functionsOnStack.add(localVariableFrame.getFunctionName());
+				localVariableFrame = localVariableFrame.iNext;
+			}//end while
+			Collections.reverse(functionsOnStack);
+			StringBuilder functionsDump = new StringBuilder();
+			functionsDump.append("(User Function Call Stack: ");
+			for(String functionName: functionsOnStack)
+			{
+				functionsDump.append(functionName + ", ");
+			}//end for.
+			functionsDump.append(")\n");
+			if (TRACE_TO_STANDARD_OUT) {
+				System.out.print(functionsDump.toString());
+			} else {
+				aEnvironment.write(functionsDump.toString());
+			}//end else.
+		}//end if.
 
 
-        evalDepth++;
-    }//end method.
+		evalDepth++;
+	}//end method.
 
-    public static void traceShowArg(Environment aEnvironment, ConsPointer aParam, ConsPointer aValue) throws Exception {
-        for (int i = 0; i < evalDepth; i++) {
-            //aEnvironment.iEvalDepth; i++) {
-            if (TRACE_TO_STANDARD_OUT) {
-                System.out.print("    ");
-            } else {
-                aEnvironment.write("    ");
-            }
-        }
-        if (TRACE_TO_STANDARD_OUT) {
-            System.out.print("Arg(");
-        } else {
-            aEnvironment.write("Arg(");
-        }
-        traceShowExpression(aEnvironment, aParam);
-        if (TRACE_TO_STANDARD_OUT) {
-            System.out.print(" -> ");
-        } else {
-            aEnvironment.write(" -> ");
-        }
-        traceShowExpression(aEnvironment, aValue);
-        if (TRACE_TO_STANDARD_OUT) {
-            System.out.print(");\n");
-        } else {
-            aEnvironment.write(");\n");
-        }
-    }//end method.
+	public static void traceShowArg(Environment aEnvironment, ConsPointer aParam, ConsPointer aValue) throws Exception {
+		for (int i = 0; i < evalDepth; i++) {
+			//aEnvironment.iEvalDepth; i++) {
+			if (TRACE_TO_STANDARD_OUT) {
+				System.out.print("    ");
+			} else {
+				aEnvironment.write("    ");
+			}
+		}
+		if (TRACE_TO_STANDARD_OUT) {
+			System.out.print("Arg(");
+		} else {
+			aEnvironment.write("Arg(");
+		}
+		traceShowExpression(aEnvironment, aParam);
+		if (TRACE_TO_STANDARD_OUT) {
+			System.out.print(" -> ");
+		} else {
+			aEnvironment.write(" -> ");
+		}
+		traceShowExpression(aEnvironment, aValue);
+		if (TRACE_TO_STANDARD_OUT) {
+			System.out.print(");\n");
+		} else {
+			aEnvironment.write(");\n");
+		}
+	}//end method.
 
-    public static void traceShowExpression(Environment aEnvironment, ConsPointer aExpression) throws Exception {
-        StringBuffer outString = new StringBuffer();
+	public static void traceShowExpression(Environment aEnvironment, ConsPointer aExpression) throws Exception {
+		StringBuffer outString = new StringBuffer();
 
-        showExpression(outString, aEnvironment, aExpression);
+		showExpression(outString, aEnvironment, aExpression);
 
-        String expression = outString.toString();
-        expression = expression.replace("\n", "");
+		String expression = outString.toString();
+		expression = expression.replace("\n", "");
 
-        if (TRACE_TO_STANDARD_OUT) {
-            System.out.print(expression);
-        } else {
-            aEnvironment.write(expression);
-        }
-    }//end method.
+		if (TRACE_TO_STANDARD_OUT) {
+			System.out.print(expression);
+		} else {
+			aEnvironment.write(expression);
+		}
+	}//end method.
 
-    public static void traceShowRule(Environment aEnvironment, ConsPointer aExpression, String ruleDump) throws Exception {
+	public static void traceShowRule(Environment aEnvironment, ConsPointer aExpression, String ruleDump) throws Exception {
 
-        for (int i = 0; i < evalDepth; i++) {
-            // aEnvironment.iEvalDepth; i++) {
-            if (TRACE_TO_STANDARD_OUT) {
-                System.out.print("    ");
-            } else {
-                aEnvironment.write("    ");
-            }
-        }
+		for (int i = 0; i < evalDepth; i++) {
+			// aEnvironment.iEvalDepth; i++) {
+			if (TRACE_TO_STANDARD_OUT) {
+				System.out.print("    ");
+			} else {
+				aEnvironment.write("    ");
+			}
+		}
 
-        String function = "";
-        if (aExpression.getCons().getSublistPointer() != null) {
-            ConsPointer sub = aExpression.getCons().getSublistPointer();
-            if (sub.getCons().string() != null) {
-                function = sub.getCons().string();
-            }
-        }//end function.
+		String function = "";
+		if (aExpression.getCons().getSublistPointer() != null) {
+			ConsPointer sub = aExpression.getCons().getSublistPointer();
+			if (sub.getCons().string() != null) {
+				function = sub.getCons().string();
+			}
+		}//end function.
 
-        ruleDump = ruleDump.replace("\n", "");
+		ruleDump = ruleDump.replace("\n", "");
 
-        if (TRACE_TO_STANDARD_OUT) {
-            System.out.print("**** Rule in function (" + function + ") matched: ");
-            System.out.print(ruleDump);
-            System.out.print("\n");
-        } else {
-            aEnvironment.write("**** Rule in function (" + function + ") matched: ");
-            aEnvironment.write(ruleDump);
-            aEnvironment.write("\n");
-        }
-    }//end method.
+		if (TRACE_TO_STANDARD_OUT) {
+			System.out.print("**** Rule in function (" + function + ") matched: ");
+			System.out.print(ruleDump);
+			System.out.print("\n");
+		} else {
+			aEnvironment.write("**** Rule in function (" + function + ") matched: ");
+			aEnvironment.write(ruleDump);
+			aEnvironment.write("\n");
+		}
+	}//end method.
 
-    public static void traceShowLeave(Environment aEnvironment, ConsPointer aResult, ConsPointer aExpression, String extraInfo, String localVariables) throws Exception {
-        if (evalDepth != 0) {
-            evalDepth--;
-        }
-        for (int i = 0; i < evalDepth; i++) {
-            // aEnvironment.iEvalDepth; i++) {
-            if (TRACE_TO_STANDARD_OUT) {
-                System.out.print("    ");
-            } else {
-                aEnvironment.write("    ");
-            }
-        }
-        if (TRACE_TO_STANDARD_OUT) {
-            System.out.print("Leave<" + extraInfo + ">}(");
-        } else {
-            aEnvironment.write("Leave<" + extraInfo + ">}(");
-        }
-        traceShowExpression(aEnvironment, aExpression);
-        if (TRACE_TO_STANDARD_OUT) {
-            System.out.print(" -> ");
-        } else {
-            aEnvironment.write(" -> ");
-        }
+	public static void traceShowLeave(Environment aEnvironment, ConsPointer aResult, ConsPointer aExpression, String extraInfo, String localVariables) throws Exception {
+		if (evalDepth != 0) {
+			evalDepth--;
+		}
+		for (int i = 0; i < evalDepth; i++) {
+			// aEnvironment.iEvalDepth; i++) {
+			if (TRACE_TO_STANDARD_OUT) {
+				System.out.print("    ");
+			} else {
+				aEnvironment.write("    ");
+			}
+		}
+		if (TRACE_TO_STANDARD_OUT) {
+			System.out.print("Leave<" + extraInfo + ">}(");
+		} else {
+			aEnvironment.write("Leave<" + extraInfo + ">}(");
+		}
+		traceShowExpression(aEnvironment, aExpression);
+		if (TRACE_TO_STANDARD_OUT) {
+			System.out.print(" -> ");
+		} else {
+			aEnvironment.write(" -> ");
+		}
 
-        traceShowExpression(aEnvironment, aResult);
+		traceShowExpression(aEnvironment, aResult);
 
-        if (localVariables != null) {
-            if (TRACE_TO_STANDARD_OUT) {
-                System.out.print(",    " + localVariables);
-            } else {
-                aEnvironment.write(",    " + localVariables);
-            }//end else.
+		if (localVariables != null) {
+			if (TRACE_TO_STANDARD_OUT) {
+				System.out.print(",    " + localVariables);
+			} else {
+				aEnvironment.write(",    " + localVariables);
+			}//end else.
 
-        }//end if.
+		}//end if.
 
-        if (TRACE_TO_STANDARD_OUT) {
-            System.out.print(");\n");
-        } else {
-            aEnvironment.write(");\n");
-        }//end else.
-    }//end method.
+		if (TRACE_TO_STANDARD_OUT) {
+			System.out.print(");\n");
+		} else {
+			aEnvironment.write(");\n");
+		}//end else.
+	}//end method.
 
-    public static boolean isTraced() {
-        return iTraced;
-    }
+	public static boolean isTraced() {
+		return iTraced;
+	}
 
-    public static void traceOff() {
-        iTraced = false;
-    }
+	public static void traceOff() {
+		iTraced = false;
+	}
 
-    public static void traceOn() {
-        iTraced = true;
-    }
+	public static void traceOn() {
+		iTraced = true;
+	}
 
-    public abstract void evaluate(Environment aEnvironment, ConsPointer aResult, ConsPointer aArgumentsOrExpression) throws Exception;
+	public abstract void evaluate(Environment aEnvironment, ConsPointer aResult, ConsPointer aArgumentsOrExpression) throws Exception;
 
-    public void resetStack() {
-    }
+	public void resetStack() {
+	}
 
-    public UserStackInformation stackInformation() {
-        return iBasicInfo;
-    }
+	public UserStackInformation stackInformation() {
+		return iBasicInfo;
+	}
 
-    public void showStack(Environment aEnvironment, MathPiperOutputStream aOutput) {
-    }//end method.
+	public void showStack(Environment aEnvironment, MathPiperOutputStream aOutput) {
+	}//end method.
 
-    public static void setTraceFunctionList(List traceFunctionList) {
-        Evaluator.traceFunctionList = traceFunctionList;
-    }
+	public static void setTraceFunctionList(List traceFunctionList) {
+		Evaluator.traceFunctionList = traceFunctionList;
+	}
 
-    public static void setTraceExceptFunctionList(List traceExceptFunctionList) {
-        Evaluator.traceExceptFunctionList = traceExceptFunctionList;
-    }
+	public static void setTraceExceptFunctionList(List traceExceptFunctionList) {
+		Evaluator.traceExceptFunctionList = traceExceptFunctionList;
+	}
 
-    public static boolean isTraceFunction(String functionName) {
-        if (!(traceFunctionList == null)) {
-            return traceFunctionList.contains(functionName);
-        } else if (!(traceExceptFunctionList == null)) {
-            return !traceExceptFunctionList.contains(functionName);
-        } else {
-            return true;
-        }//end else.
+	public static boolean isTraceFunction(String functionName) {
+		if (!(traceFunctionList == null)) {
+			return traceFunctionList.contains(functionName);
+		} else if (!(traceExceptFunctionList == null)) {
+			return !traceExceptFunctionList.contains(functionName);
+		} else {
+			return true;
+		}//end else.
 
-    }//end method.
+	}//end method.
 }//end class.
