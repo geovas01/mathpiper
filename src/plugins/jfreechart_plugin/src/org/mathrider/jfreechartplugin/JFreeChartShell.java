9e0cec4ac368dcf86ce3ae48b1401dccafeca7a3
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2009-07-06 03:28:07 +0000

    Adding a JFreeChart plugin to MathRider.

---
+++ src/plugins/jfreechart_plugin/src/org/mathrider/jfreechartplugin/JFreeChartShell.java
@@ -0,0 +1,174 @@
+//Copyright (C) 2008 Ted Kosan (license information is at the end of this document.)
+/* {{{ License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ //}}}
+
+// :indentSize=4:lineSeparator=\n:noTabs=false:tabSize=4:folding=explicit:collapseFolds=1:
+
+package org.mathrider.jfreechartplugin;
+
+import javax.script.*;
+import org.armedbear.lisp.*;
+
+import java.awt.Color;
+import java.io.*;
+
+
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.EditPlugin;
+
+import console.Console;
+import console.ConsolePane;
+import console.Output;
+import console.Shell;
+
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
+
+public class JFreeChartShell extends Shell
+{
+	private static ScriptEngine interpreter;
+	//private static Interpreter interpreter;
+
+	public JFreeChartShell() //throws org.mathrider.jfreechart.Piperexception
+	{
+		super("JFreeChart");
+		getInterpreter();
+
+	}//end constructor.
+	
+	public static synchronized ScriptEngine getInterpreter()
+	{
+		if(interpreter == null)
+		{
+			interpreter = new ScriptEngineManager().getEngineByExtension("lisp");
+			//interpreter = Interpreter.createInstance();
+			return interpreter;
+		}
+		else
+		{
+			return interpreter;
+		}
+	}//end method.
+
+
+
+	//{{{Begin Shell implementation.
+	public void printInfoMessage(Output output)
+	{
+
+		//printUsage(jEdit.getProperty(JFreeChartPlugin.NAME + ".shell.msg.info"), null, output);
+		//output.print(null, jEdit.getProperty(JFreeChartPlugin.NAME + ".shell.msg.usage"));
+		//try
+		//{
+		//	output.print(null, "Piper_ME version " + interpreter.evaluate("Version();") );
+		//}
+		//catch( org.mathrider.jfreechart.Piperexception pe )
+		//{
+		//	output.print(java.awt.Color.RED,pe.getMessage() );
+		//}
+
+	}//end method.
+
+	public void execute(Console console, String input, Output output, Output error, String script)
+	{
+
+		try {
+			
+			Object response = interpreter.eval(script);
+			
+			if(response instanceof LispObject)
+			{
+				LispObject lispObject = (LispObject) response;
+				String responseString = lispObject.writeToString();
+				responseString = responseString.replaceAll("JFreeChart-SCRIPT-USER::","");
+				output.print(null,  responseString);
+			}
+			else
+			{
+				output.print(null,  response.toString());
+			}
+			
+		}
+		catch(Throwable e)
+		{
+
+			output.print(java.awt.Color.RED,e.getMessage() );
+			
+			e.printStackTrace();
+		}
+		finally {
+			//Var.popThreadBindings();
+			output.commandDone();
+		}
+
+
+
+
+
+	}//end method.
+
+	public void stop(Console console)
+	{
+
+	}
+
+	public boolean waitFor(Console console)
+	{
+
+		return true;
+	}//end method.
+
+
+	public void printPrompt(Console console, Output output)
+	{
+		output.writeAttrs(ConsolePane.colorAttributes(console.getPlainColor()), "\n=> ");
+	}//end method.
+
+
+
+	// }}}End Shell implementation
+
+
+
+	//{{{ Print usage methods.
+	private void printCurrentProjectInfo(Color color, Output output)
+	{
+
+	}
+
+	private String printDefaultLabel(String target)
+	{
+
+		return "";
+	}
+
+	private void printUsage(String additionalMessage, Color color, Output output)
+	{
+		//output.print(color, additionalMessage);
+		//output.print(null, jEdit.getProperty(AntFarmPlugin.NAME + ".shell.msg.usage"));
+	}
+
+	//}}}
+
+}//end class.
+
+
+
