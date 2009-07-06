a95ca3cd2d85d4d31450f3ec2b02db6127cfcaab
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2009-07-06 20:58:02 +0000

    Beginning to add the ability to instantiate java objects.

---
+++ trunk/src/library_apps/mathpiper/src/org/mathpiper/builtin/functions/optional/JavaNew.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.mathpiper.builtin.functions.optional;
+
+/**
+ *
+ * @author tkosan
+ */
+public class JavaNew {
+
+}
