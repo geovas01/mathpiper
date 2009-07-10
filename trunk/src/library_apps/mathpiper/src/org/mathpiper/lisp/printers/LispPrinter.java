55b12642aff110bdb844b2a163cf305a246bef70
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2009-07-10 23:53:48 +0000

    Formatted source code.

--- trunk/src/library_apps/mathpiper/src/org/mathpiper/lisp/printers/LispPrinter.java
+++ trunk/src/library_apps/mathpiper/src/org/mathpiper/lisp/printers/LispPrinter.java
@@ -13,73 +13,58 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */ //}}}
-
 // :indentSize=4:lineSeparator=\n:noTabs=false:tabSize=4:folding=explicit:collapseFolds=0:
-
 package org.mathpiper.lisp.printers;
 
 import org.mathpiper.lisp.cons.ConsPointer;
 import org.mathpiper.lisp.*;
 import org.mathpiper.io.MathPiperOutputStream;
 
+public class LispPrinter {
 
-public class LispPrinter
-{
-	public void print(ConsPointer aExpression, MathPiperOutputStream aOutput, Environment aEnvironment) throws Exception
-	{
-		printExpression(aExpression, aOutput, aEnvironment,0);
-	}
-	public void rememberLastChar(char aChar)
-	{
-	}
+    public void print(ConsPointer aExpression, MathPiperOutputStream aOutput, Environment aEnvironment) throws Exception {
+        printExpression(aExpression, aOutput, aEnvironment, 0);
+    }
 
-	void printExpression(ConsPointer aExpression, MathPiperOutputStream aOutput,
-	                     Environment aEnvironment,int aDepth /* =0 */) throws Exception
-	{
-		ConsPointer iter = new ConsPointer();
-		iter.setCons(aExpression.getCons());
-		int item = 0;
-		while (iter.getCons() != null)
-		{
+    public void rememberLastChar(char aChar) {
+    }
 
-			
+    void printExpression(ConsPointer aExpression, MathPiperOutputStream aOutput, Environment aEnvironment, int aDepth /* =0 */) throws Exception {
+        ConsPointer consWalker = new ConsPointer();
+        consWalker.setCons(aExpression.getCons());
+        int item = 0;
 
-			if (iter.car() instanceof String)
-			{
-                String string = (String) iter.car();
-				aOutput.write(string);
-				aOutput.putChar(' ');
-			}
-			// else print "(", print sublist, and print ")"
-			else if (iter.car() instanceof ConsPointer)
-			{
-				if (item != 0)
-				{
-					indent(aOutput,aDepth+1);
-				}
-				aOutput.write("(");
-				printExpression(((ConsPointer) iter.car()),aOutput, aEnvironment,aDepth+1);
-				aOutput.write(")");
-				item=0;
-			}
-			else
-			{
-				aOutput.write("[BuiltinObject]");
-			}
-			iter = (iter.cdr());
-			item++;
-		} // print rest element
-	}
+        while (consWalker.getCons() != null) {
 
-	void indent(MathPiperOutputStream aOutput, int aDepth) throws Exception
-	{
-		aOutput.write("\n");
-		int i;
-		for (i=aDepth;i>0;i--)
-		{
-			aOutput.write("  ");
-		}
-	}
+            if (consWalker.car() instanceof String) {
+                String string = (String) consWalker.car();
+                aOutput.write(string);
+                aOutput.putChar(' ');
+            } // else print "(", print sublist, and print ")"
+            else if (consWalker.car() instanceof ConsPointer) {
+                if (item != 0) {
+                    indent(aOutput, aDepth + 1);
+                }
+                aOutput.write("(");
+                printExpression(((ConsPointer) consWalker.car()), aOutput, aEnvironment, aDepth + 1);
+                aOutput.write(")");
+                item = 0;
+            } else {
+                aOutput.write("[BuiltinObject]");
+            }
+            consWalker = (consWalker.cdr()); // print rest element
+            item++;
+        }//end while.
+        
+    }//end method.
+
+    void indent(MathPiperOutputStream aOutput, int aDepth) throws Exception {
+        aOutput.write("\n");
+        int i;
+        for (i = aDepth; i > 0; i--) {
+            aOutput.write("  ");
+        }
+    }
 };
 
 
