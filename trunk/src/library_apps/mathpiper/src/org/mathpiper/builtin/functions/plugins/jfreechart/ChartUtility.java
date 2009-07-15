bb5d02e49fb889dbb06cccd7ef8c791c895a1ea1
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2009-07-15 02:46:03 +0000

    ScatterPlot now works.

--- trunk/src/library_apps/mathpiper/src/org/mathpiper/builtin/functions/plugins/jfreechart/ChartUtility.java
+++ trunk/src/library_apps/mathpiper/src/org/mathpiper/builtin/functions/plugins/jfreechart/ChartUtility.java
@@ -14,72 +14,72 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */ //}}}
 // :indentSize=4:lineSeparator=\n:noTabs=false:tabSize=4:folding=explicit:collapseFolds=0:
-
 package org.mathpiper.builtin.functions.plugins.jfreechart;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.jfree.data.statistics.HistogramDataset;
+import org.jfree.data.xy.DefaultXYDataset;
 import org.jfree.data.xy.XYDataset;
 import org.mathpiper.builtin.JavaObject;
+import org.mathpiper.lisp.LispError;
 import org.mathpiper.lisp.Utility;
 import org.mathpiper.lisp.cons.ConsPointer;
 
 public class ChartUtility {
-    
-    public static HistogramDataset listToHistogramDataset(ConsPointer dataListPointer, Map userOptions) throws Exception{
+
+    public static HistogramDataset listToHistogramDataset(ConsPointer dataListPointer, Map userOptions) throws Exception {
 
         HistogramDataset dataSet = new HistogramDataset();
 
         if (Utility.isNestedList(dataListPointer)) {
 
-           List dataSeriesList = new ArrayList();
-           List seriesTotalList = new ArrayList();
-           dataListPointer.goNext(); //Strip List tag.
-           int seriesIndex = 1;
-            while(dataListPointer.getCons() != null)
-            {
-                 double[] dataValues = JavaObject.LispListToJavaDoubleArray((ConsPointer) dataListPointer.car());
-                 String seriesTitle = "";
-                 if(userOptions.containsKey("series" + seriesIndex + "Title"))
-                 {
-                     seriesTitle =(String) userOptions.get("series" + seriesIndex + "Title");
-                 }
-                  dataSeriesList.add(seriesTitle);
-                  dataSeriesList.add(dataValues);
-                  int index = 0;
-                  while(index < dataValues.length)
-                  {
-                        seriesTotalList.add(dataValues[index++]);
-                  }//end while
-                 seriesIndex++;
-                 dataListPointer.goNext();
+            List dataSeriesList = new ArrayList();
+            List seriesTotalList = new ArrayList();
+            dataListPointer.goNext(); //Strip List tag.
+            int seriesIndex = 1;
+            while (dataListPointer.getCons() != null) {
+                double[] dataValues = JavaObject.LispListToJavaDoubleArray((ConsPointer) dataListPointer.car());
+                String seriesTitle = "";
+                if (userOptions.containsKey("series" + seriesIndex + "Title")) {
+                    seriesTitle = (String) userOptions.get("series" + seriesIndex + "Title");
+                }
+                dataSeriesList.add(seriesTitle);
+                dataSeriesList.add(dataValues);
+                int index = 0;
+                while (index < dataValues.length) {
+                    seriesTotalList.add(dataValues[index++]);
+                }//end while
+                seriesIndex++;
+                dataListPointer.goNext();
             }//end while.
 
 
             double min = Double.MAX_VALUE;
             double max = Double.MIN_VALUE;
             int index = 0;
-            while(index < seriesTotalList.size())
-            {
+            while (index < seriesTotalList.size()) {
                 Double value = (Double) seriesTotalList.get(index);
-                if(value < min) min = value;
-                if(value > max) max = value;
+                if (value < min) {
+                    min = value;
+                }
+                if (value > max) {
+                    max = value;
+                }
                 index++;
             }//end while
             min = Math.floor(min);
             max = Math.floor(max);
 
 
-           int seriesIndex2 = 0;
-           while(seriesIndex  > 1)
-           {
-               String seriesTitle = (String) dataSeriesList.get(seriesIndex2++);
-               double[] dataValues = (double[]) dataSeriesList.get(seriesIndex2++);
-               dataSet.addSeries(seriesTitle, dataValues, 10, min, max);
-               seriesIndex--;
-           }//end while.
+            int seriesIndex2 = 0;
+            while (seriesIndex > 1) {
+                String seriesTitle = (String) dataSeriesList.get(seriesIndex2++);
+                double[] dataValues = (double[]) dataSeriesList.get(seriesIndex2++);
+                dataSet.addSeries(seriesTitle, dataValues, 10, min, max);
+                seriesIndex--;
+            }//end while.
 
         } else {//Just a single series.
             double[] dataValues = JavaObject.LispListToJavaDoubleArray(dataListPointer);
@@ -90,41 +90,37 @@ public class ChartUtility {
 
     }//end method.
 
+    public static XYDataset listToXYDataset(ConsPointer dataListPointer, Map userOptions) throws Exception {
+
+        LispError.check(Utility.isNestedList(dataListPointer), LispError.INVALID_ARGUMENT);
+
+        DefaultXYDataset dataSet = new DefaultXYDataset();
+
+        dataListPointer.goNext(); //Strip List tag.
+        int seriesIndex = 1;
+        while (dataListPointer.getCons() != null) {
+            double[] dataXValues = JavaObject.LispListToJavaDoubleArray((ConsPointer) dataListPointer.car());
+            dataListPointer.goNext();
+            double[] dataYValues = JavaObject.LispListToJavaDoubleArray((ConsPointer) dataListPointer.car());
+
+            String seriesTitle = "";
+            if (userOptions.containsKey("series" + seriesIndex + "Title")) {
+                seriesTitle = (String) userOptions.get("series" + seriesIndex + "Title");
+            }
+
+            LispError.check(dataXValues.length == dataYValues.length, LispError.INVALID_ARGUMENT);
+
+            dataSet.addSeries(seriesTitle, new double[][]{dataXValues, dataYValues});
+
+            seriesIndex++;
+            dataListPointer.goNext();
+        }//end while.
 
 
 
-    public static XYDataset listToXYDataset(ConsPointer dataListPointer, Map userOptions) throws Exception{
 
-        XYDataset dataSet = new HistogramDataset();
-
-        if (Utility.isNestedList(dataListPointer)) {
-
-           List dataSeriesList = new ArrayList();
-           dataListPointer.goNext(); //Strip List tag.
-           int seriesIndex = 1;
-            while(dataListPointer.getCons() != null)
-            {
-                 double[] dataValues = JavaObject.LispListToJavaDoubleArray((ConsPointer) dataListPointer.car());
-                 String seriesTitle = "";
-                 if(userOptions.containsKey("series" + seriesIndex + "Title"))
-                 {
-                     seriesTitle =(String) userOptions.get("series" + seriesIndex + "Title");
-                 }
-                 //dataSet.addSeries(seriesTitle, dataValues);
-                 seriesIndex++;
-                 dataListPointer.goNext();
-            }//end while.
-
-
-
-        } else {//Just a single series.
-            double[] dataValues = JavaObject.LispListToJavaDoubleArray(dataListPointer);
-            //dataSet.addSeries((String) userOptions.get("seriesTitle"), dataValues, 10);
-            //argumentsPointer.goNext();
-        }//end if/else
         return dataSet;
 
     }//end method.
-
-
 }//end class
+
