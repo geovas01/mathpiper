4314a173bbc2d785d6342c03e0895ba253454976
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2009-07-21 05:52:33 +0000

    The navigation buttons work now.

--- trunk/src/library_apps/mathpiper/src/org/mathpiper/ui/gui/help/FunctionTreePanel.java
+++ trunk/src/library_apps/mathpiper/src/org/mathpiper/ui/gui/help/FunctionTreePanel.java
@@ -19,9 +19,10 @@ package org.mathpiper.ui.gui.help;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
@@ -32,13 +33,16 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
-import javax.swing.JTree;
 import javax.swing.SwingUtilities;
 import javax.swing.ToolTipManager;
 import javax.swing.event.HyperlinkEvent;
@@ -46,6 +50,10 @@ import javax.swing.event.HyperlinkListener;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import org.mathpiper.exceptions.EvaluationException;
+import org.mathpiper.interpreters.EvaluationResponse;
+import org.mathpiper.interpreters.Interpreter;
+import org.mathpiper.interpreters.Interpreters;
 
 public class FunctionTreePanel extends JPanel implements TreeSelectionListener, HyperlinkListener {
 
@@ -57,18 +65,24 @@ public class FunctionTreePanel extends JPanel implements TreeSelectionListener, 
     private DefaultMutableTreeNode programmerFunctionsNode;
     private DefaultMutableTreeNode operatorsNode;
     private List allFunctions;
-    private JTree functionsTree;
+    private FunctionInfoTree functionsTree;
     private Map documentationIndex;
     private RandomAccessFile documentFile;
     private URL documentationURL;
     private JEditorPane editorPane;
     private StringBuilder seeFunctionsBuilder = new StringBuilder();
     private ClassLoader classLoader;
+    private List pageList;
+    private ToolPanel toolPanel = null;
+    private String selectedFunctionName = "";
 
     public FunctionTreePanel(ClassLoader classLoader) {
         this.classLoader = classLoader;
 
         this.setLayout(new BorderLayout());
+
+        pageList = new ArrayList();
+        pageList.add("HomePage");
 
         URL fileURL = classLoader.getSystemResource("org/mathpiper/ui/gui/help/data/function_categories.txt");
         if (fileURL != null) //File is on the classpath.
@@ -76,7 +90,7 @@ public class FunctionTreePanel extends JPanel implements TreeSelectionListener, 
             //System.out.println("Found categories file.");
             loadCategories(fileURL);
             loadDocumentationIndex(classLoader.getSystemResource("org/mathpiper/ui/gui/help/data/documentation_index.txt"));
-            //this.openDocumentationFile(classLoader.getSystemResource("org/mathpiper/ui/gui/help/data/documentation.txt"));
+
             documentationURL = classLoader.getSystemResource("org/mathpiper/ui/gui/help/data/documentation.txt");
 //System.out.println(documentationURL);
 
@@ -110,6 +124,9 @@ public class FunctionTreePanel extends JPanel implements TreeSelectionListener, 
             //tree.getPreferredScrollableViewportSize().width;
             splitPane.setDividerLocation(290);
             this.add(splitPane);
+
+            toolPanel = new ToolPanel();
+            home();
 
         }//end if.
     }//end constructor.
@@ -345,27 +362,6 @@ public class FunctionTreePanel extends JPanel implements TreeSelectionListener, 
         }
     }//end method.
 
-    private void openDocumentationFile(URL url) {
-
-        //System.out.println("openDocumentationFile: " + url);
-
-       /* try {
-
-
-            docsStream = new BufferedInputStream(url.openStream());
-
-            //documentFile = new RandomAccessFile(new File(url.toURI()), "r");
-
-       // } catch (URISyntaxException e) {
-            //e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-    }//end method.
-
     public void valueChanged(TreeSelectionEvent e) {
         DefaultMutableTreeNode node = (DefaultMutableTreeNode) functionsTree.getLastSelectedPathComponent();
         //System.out.println("XXXXX");
@@ -376,62 +372,49 @@ public class FunctionTreePanel extends JPanel implements TreeSelectionListener, 
 
         Object nodeInfo = node.getUserObject();
         if (node.isLeaf()) {
-            String functionName = nodeInfo.toString();
-            viewFunction(functionName);
+            selectedFunctionName = nodeInfo.toString();
+            viewFunction(selectedFunctionName, true);
         } else {
             //toolPanel.sourceButtonEnabled(false);
             //Note:tk:Perhaps display top of chapter here?
         }
     }//end method.
 
+    private void viewFunction(String functionName, boolean save) {
+        String functionIndexesString = (String) this.documentationIndex.get(functionName);
+        String[] functionIndexes = functionIndexesString.split(",");
+        int startIndex = Integer.parseInt(functionIndexes[0]);
+        int endIndex = Integer.parseInt(functionIndexes[1]);
+        int length = endIndex - startIndex;
+        byte[] documentationData = new byte[length];
+        //char[] documentationData = new char[length];
+        //System.out.println("yyyy " + functionName + "  " + startIndex + " " + endIndex + " " + length);
+        try {
+
+            //documentFile.seek(startIndex);
+            //documentFile.read(documentationData, 0, length);
+
+            // System.out.print("docsurl: " + documentationURL);
+            BufferedInputStream docsStream = new BufferedInputStream(documentationURL.openStream());
+            docsStream.skip(startIndex);
+            docsStream.read(documentationData, 0, length);
+            docsStream.close();
 
 
-    private void viewFunction(String functionName)
-    {
-            String functionIndexesString = (String) this.documentationIndex.get(functionName);
-            String[] functionIndexes = functionIndexesString.split(",");
-            int startIndex = Integer.parseInt(functionIndexes[0]);
-            int endIndex = Integer.parseInt(functionIndexes[1]);
-            int length = endIndex - startIndex;
-            byte[] documentationData = new byte[length];
-            //char[] documentationData = new char[length];
-            //System.out.println("yyyy " + functionName + "  " + startIndex + " " + endIndex + " " + length);
-            try {
+            String documentationDataString = new String(documentationData);
 
-                //documentFile.seek(startIndex);
-                //documentFile.read(documentationData, 0, length);
+            documentationDataString = documentationDataString.replace("$", "");
 
-       // System.out.print("docsurl: " + documentationURL);
-                BufferedInputStream docsStream = new BufferedInputStream(documentationURL.openStream());
-                docsStream.skip(startIndex);
-                docsStream.read(documentationData,0,length);
-                docsStream.close();
+            String html = textToHtml(documentationDataString);
 
+            setPage(functionName, html, save);
 
-                String documentationDataString = new String(documentationData);
-
-                documentationDataString = documentationDataString.replace("$", "");
-
-                String html = textToHtml(documentationDataString);
-                editorPane.setText(html);
-                //editorPane.validate();
-
-                final JScrollBar verticalScrollBar = docsScrollPane.getVerticalScrollBar();
-                SwingUtilities.invokeLater(new Runnable() {
-
-                    public void run() {
-                        verticalScrollBar.setValue(verticalScrollBar.getMinimum());
-                    }
-                });
-
-                //functionInfo = nodeInfo;
-                //displayFunctionDocs(functionInfo.toString());
+            //functionInfo = nodeInfo;
+            //displayFunctionDocs(functionInfo.toString());
             } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+            ex.printStackTrace();
+        }
     }//end method.
-
-
 
     private String applyBold(String line) {
         line = line.replaceAll("\\{", "<b><tt>");
@@ -439,8 +422,6 @@ public class FunctionTreePanel extends JPanel implements TreeSelectionListener, 
         return line;
     }//end method.
 
-
-    
     private String textToHtml(String s) {
 //s = "*CMD D --- take derivative of expression with respect to variable\n*STD\n*CALL\n	D(variable) expression\n	D(list) expression\n	D(variable,n) expression\n\n*PARMS\n\n{variable} -- variable\n\n{list} -- a list of variables\n\n{expression} -- expression to take derivatives of\n\n{n} -- order of derivative\n\n*DESC\n\nThis function calculates the derivative of the expression {expr} with\nrespect to the variable {var} and returns it. If the third calling\nformat is used, the {n}-th derivative is determined. Yacas knows\nhow to differentiate standard functions such as {Ln}\nand {Sin}.\n\nThe {D} operator is threaded in both {var} and\n{expr}. This means that if either of them is a list, the function is\napplied to each entry in the list. The results are collected in\nanother list which is returned. If both {var} and {expr} are a\nlist, their lengths should be equal. In this case, the first entry in\nthe list {expr} is differentiated with respect to the first entry in\nthe list {var}, the second entry in {expr} is differentiated with\nrespect to the second entry in {var}, and so on.\n\nThe {D} operator returns the original function if $n=0$, a common\nmathematical idiom that simplifies many formulae.\n\n*E.G.\n\n	In> D(x)Sin(x*y)\n	Out> y*Cos(x*y);\n	In> D({x,y,z})Sin(x*y)\n	Out> {y*Cos(x*y),x*Cos(x*y),0};\n	In> D(x,2)Sin(x*y)\n	Out> -Sin(x*y)*y^2;\n	In> D(x){Sin(x),Cos(x)}\n	Out> {Cos(x),-Sin(x)};\n\n*SEE Integrate, Taylor, Diverge, Curl\n";
 
@@ -614,26 +595,228 @@ public class FunctionTreePanel extends JPanel implements TreeSelectionListener, 
         if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
 
             String urlString = url.toString();
-            String functionName = urlString.substring(7,urlString.length());
+            String functionName = urlString.substring(7, urlString.length());
             //System.out.println(functionName);
-            viewFunction(functionName);
+            viewFunction(functionName, true);
 
 
 
         }//end if.  + getRef())
 
     }//end method.
+    private int pageIndex = -1;
+
+    private void setPage(String functionName, String html, boolean save) {
+        editorPane.setText(html);
+
+
+
+        //forward button logic.
+        if (pageIndex + 1 == pageList.size()) {
+            toolPanel.forwardButtonEnabled(false);
+        } else {
+            toolPanel.forwardButtonEnabled(true);
+        }//end else.
+
+
+
+
+        if (save) {
+            if (pageIndex >= 0 && functionName == pageList.get(pageIndex)) {
+                //System.out.println("VVVVVM Same URL");
+                return;
+            }//end if.
+            if (pageIndex + 1 != pageList.size()) {
+                pageList = pageList.subList(0, pageIndex + 1);
+                toolPanel.forwardButtonEnabled(false);
+            }//end if.
+
+
+            pageList.add(functionName);
+            pageIndex++;
+        }//end if.
+
+
+        //back button logic.
+        if (pageIndex <= 0) {
+            toolPanel.backButtonEnabled(false);
+        } else {
+            toolPanel.backButtonEnabled(true);
+        }//end else.
+
+        //System.out.println("TTTTT " + pageList );
+
+
+        final JScrollBar verticalScrollBar = docsScrollPane.getVerticalScrollBar();
+        SwingUtilities.invokeLater(new Runnable() {
+
+            public void run() {
+                verticalScrollBar.setValue(verticalScrollBar.getMinimum());
+            }
+        });
+
+
+    }//end method.
+
+    private String getSource() {
+        	Interpreter interpreter = Interpreters.getSynchronousInterpreter();
+	//item = list.getSelectedValue();
+
+		EvaluationResponse evaluationResponse = interpreter.evaluate("FindFunction(\"" + selectedFunctionName + "\");");
+		String location = evaluationResponse.getResult();
+
+                return location;
+    }//end method.
+
+
+
+    
+    private void collapse() {
+        functionsTree.collapseAll();
+    }//end method.
+
+    private void back() {
+        if (pageIndex != 0) {
+            String functionName = (String) pageList.get(--pageIndex);
+
+            if (functionName.equals("HomePage")) {
+                home();
+            } else {
+                this.viewFunction(functionName, false);
+            }
+
+        }//end if.
+    }//end method.
+
+    private void forward() {
+        String functionName = (String) pageList.get(++pageIndex);
+
+        if (functionName.equals("HomePage")) {
+            home();
+        } else {
+            this.viewFunction(functionName, false);
+        }
+
+    }//end method.
+
+    private void home() {
+        toolPanel.sourceButtonEnabled(false);
+
+        setPage("HomePage", "<html>Home page</html>", true);
+    }//end method.
+
+    public JPanel getToolPanel() {
+        return toolPanel;
+    }//end method.
+
+    private class ToolPanel extends JPanel {
+
+        private JLabel label;
+        private JButton sourceButton;
+        private JButton collapseButton;
+        private JButton backButton;
+        private JButton forwardButton;
+        private JButton homeButton;
+
+        private ToolPanel() {
+            setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
+
+
+            //View source button.
+            sourceButton = new javax.swing.JButton("Source");
+            sourceButton.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent evt) {
+                    //source();
+                }
+            });
+            sourceButton.setEnabled(false);
+            sourceButton.setToolTipText("View script source.");
+            add(sourceButton);
+
+
+            //Collapse tree button.
+            collapseButton = new javax.swing.JButton("Collapse");
+            collapseButton.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent evt) {
+                    collapse();
+                }
+            });
+            collapseButton.setEnabled(true);
+            collapseButton.setToolTipText("Collapse function tree.");
+            add(collapseButton);
+
+
+
+            add(Box.createGlue());
+
+
+            //back button.
+            backButton = new javax.swing.JButton("Back");
+            backButton.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent evt) {
+                    back();
+                }
+            });
+            backButton.setEnabled(false);
+            add(backButton);
+
+
+            //forward button.
+            forwardButton = new javax.swing.JButton("Forward");
+            forwardButton.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent evt) {
+                    forward();
+                }
+            });
+            forwardButton.setEnabled(false);
+            add(forwardButton);
+
+
+            //Home button.
+            homeButton = new javax.swing.JButton("Home");
+            homeButton.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent evt) {
+                    home();
+                }
+            });
+            homeButton.setEnabled(true);
+            add(homeButton);
+
+
+
+
+        }//end constructor.
+
+        public void sourceButtonEnabled(Boolean state) {
+            sourceButton.setEnabled(state);
+        }//end method.
+
+        public void backButtonEnabled(Boolean state) {
+            backButton.setEnabled(state);
+        }//end method.
+
+        public void forwardButtonEnabled(Boolean state) {
+            forwardButton.setEnabled(state);
+        }//end method.
+    }//end class.
 
     public static void main(String[] args) {
 
         JFrame frame = new javax.swing.JFrame();
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        JPanel helpPanel = new FunctionTreePanel(FunctionTreePanel.class.getClassLoader());
+        FunctionTreePanel helpPanel = new FunctionTreePanel(FunctionTreePanel.class.getClassLoader());
 
 
         Container contentPane = frame.getContentPane();
-        contentPane.add(helpPanel);
+        contentPane.add(helpPanel.getToolPanel(), BorderLayout.NORTH);
+        contentPane.add(helpPanel, BorderLayout.CENTER);
 
         frame.pack();
 //frame.setAlwaysOnTop(true);
