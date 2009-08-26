c240036ed7addc9e5bc6468f1db45576d0550b36
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2009-08-26 20:42:10 +0000

    Readding the 3d-xplormath-j plugin.

---
+++ src/plugins/threedxplormathj_plugin/src/org/mathrider/threedxplormathjplugin/ThreeDXplorMathJToolPanel.java
@@ -0,0 +1,97 @@
+//Copyright (C) 2008 Ted Kosan (license information is at the end of this document.)
+
+package org.mathrider.threedxplormathjplugin;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.AbstractButton;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.gjt.sp.jedit.GUIUtilities;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.gui.RolloverButton;
+
+public class ThreeDXplorMathJToolPanel extends JPanel {
+	private ThreeDXplorMathJ pad;
+
+	private JLabel label;
+
+	public ThreeDXplorMathJToolPanel(ThreeDXplorMathJ qnpad) {
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		pad = qnpad;
+
+		Box labelBox = new Box(BoxLayout.Y_AXIS);
+		labelBox.add(Box.createGlue());
+
+		label = new JLabel(pad.getFilename());
+		label.setVisible(jEdit.getProperty(
+				ThreeDXplorMathJPlugin.OPTION_PREFIX + "show-filepath").equals(
+				"true"));
+
+		labelBox.add(label);
+		labelBox.add(Box.createGlue());
+
+		add(labelBox);
+
+		add(Box.createGlue());
+
+		add(makeCustomButton("threedxplormathj.choose-file", new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				ThreeDXplorMathJToolPanel.this.pad.chooseFile();
+			}
+		}));
+		add(makeCustomButton("threedxplormathj.save-file", new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				ThreeDXplorMathJToolPanel.this.pad.saveFile();
+			}
+		}));
+		add(makeCustomButton("threedxplormathj.copy-to-buffer",
+				new ActionListener() {
+					public void actionPerformed(ActionEvent evt) {
+						ThreeDXplorMathJToolPanel.this.pad.copyToBuffer();
+					}
+				}));
+	}
+
+	void propertiesChanged() {
+		label.setText(pad.getFilename());
+		label.setVisible(jEdit.getProperty(
+				ThreeDXplorMathJPlugin.OPTION_PREFIX + "show-filepath").equals(
+				"true"));
+	}
+
+	private AbstractButton makeCustomButton(String name, ActionListener listener) {
+		String toolTip = jEdit.getProperty(name.concat(".label"));
+		AbstractButton b = new RolloverButton(GUIUtilities.loadIcon(jEdit
+				.getProperty(name + ".icon")));
+		if (listener != null) {
+			b.addActionListener(listener);
+			b.setEnabled(true);
+		} else {
+			b.setEnabled(false);
+		}
+		b.setToolTipText(toolTip);
+		return b;
+	}
+
+}//end class.
+
+/* {{{ License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ //}}}
