2ac370cc816e4ebb970aafb750d324af3118a721
tkosan <tkosan@613135f6-e55d-11de-9043-17b7bd24f792>
2009-11-28 00:19:12 +0000

    Changed ColorConsole to Console.

--- src/plugins/mathpiper_plugin/src/org/mathrider/mathpiperplugin/MathPiper.java
+++ src/plugins/mathpiper_plugin/src/org/mathrider/mathpiperplugin/MathPiper.java
@@ -30,7 +30,7 @@ import org.gjt.sp.jedit.msg.PropertiesChanged;
 import org.gjt.sp.util.Log;
 import org.gjt.sp.util.StandardUtilities;
 import org.mathrider.mathpiperplugin.MathPiperInterpreter;
-import org.mathpiper.ui.gui.consoles.ColorConsole;
+import org.mathpiper.ui.gui.consoles.Console;
 
 // }}}
 
@@ -60,7 +60,7 @@ public class MathPiper extends JPanel
 	
 	private JButton haltCurrentExecutionButton;
 	
-	private ColorConsole console;
+	private Console console;
 
     // }}}
 
@@ -121,7 +121,7 @@ public class MathPiper extends JPanel
 		
 		//add(BorderLayout.CENTER, new org.mathpiper.ui.gui.calculator.CalculatorPanel() );
 		
-		console = new org.mathpiper.ui.gui.consoles.ColorConsole();
+		console = new org.mathpiper.ui.gui.consoles.Console();
 		
 		add(BorderLayout.CENTER, console );
 		
