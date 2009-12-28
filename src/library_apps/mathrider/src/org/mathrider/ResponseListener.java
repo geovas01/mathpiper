b6d26285d3b9532e8095350e2aff1450931ed164
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2009-12-28 21:25:43 +0000

    This library code is no longer needed because we are accessing GeoGebra as a JPanel now instead of as an applet.

--- src/library_apps/mathrider/src/org/mathrider/ResponseListener.java
+++
@@ -1,8 +0,0 @@
-package org.mathrider;
-
-public interface ResponseListener
-{
-    void response(String response);
-    void response(java.util.HashMap response);
-    boolean remove();
-}// end interface.
