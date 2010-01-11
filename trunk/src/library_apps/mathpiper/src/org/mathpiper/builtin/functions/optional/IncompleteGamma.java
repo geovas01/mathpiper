25c4e07921d3631e6ea8f5a77a0241f1002647d3
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2010-01-11 06:42:25 +0000

    Adding a built in IncompleteGamma function because the IncompleteGamma function in the scripts has bugs in it.

---
+++ trunk/src/library_apps/mathpiper/src/org/mathpiper/builtin/functions/optional/IncompleteGamma.java
@@ -0,0 +1,65 @@
+
+
+package org.mathpiper.builtin.functions.optional;
+
+import org.mathpiper.builtin.BigNumber;
+import org.mathpiper.builtin.BuiltinFunction;
+import org.mathpiper.builtin.BuiltinFunctionEvaluator;
+import org.mathpiper.builtin.library.cern.Gamma;
+import org.mathpiper.lisp.Environment;
+
+
+
+public class IncompleteGamma extends BuiltinFunction{
+
+    public void plugIn(Environment aEnvironment)
+    {
+        aEnvironment.getBuiltinFunctions().setAssociation(
+                new BuiltinFunctionEvaluator(this, 2, BuiltinFunctionEvaluator.Fixed | BuiltinFunctionEvaluator.Function),
+                "IncompleteGamma");
+    }//end method.
+
+    public void evaluate(Environment aEnvironment, int aStackTop) throws Exception
+    {
+        BigNumber a = org.mathpiper.lisp.Utility.getNumber(aEnvironment, aStackTop, 1);
+
+        BigNumber x = org.mathpiper.lisp.Utility.getNumber(aEnvironment, aStackTop, 2);
+
+        double resultValue = Gamma.incompleteGamma(x.toDouble(),  a.toDouble());
+
+        BigNumber result = new BigNumber(aEnvironment.getPrecision());
+
+        result.setTo(resultValue);
+
+        getTopOfStackPointer(aEnvironment, aStackTop).setCons(new org.mathpiper.lisp.cons.NumberCons(aEnvironment, result));
+
+    }//end method.
+
+}//end class.
+
+
+
+
+/*
+%mathpiper_docs,name="IncompleteGamma",categories="User Functions;Statistics & Probability"
+*CMD IncompleteGamma --- the incomplete gamma function
+*CORE
+*CALL
+    IncompleteGamma(a, x)
+
+*PARMS
+{a} -- the parameter of the gamma distribution
+
+{x} -- the integration end point
+
+*DESC
+
+The incomplete gamma function.
+
+*E.G.
+In> IncompleteGamma(2.5,3.6)
+Result> 0.3188972206
+
+
+%/mathpiper_docs
+*/
