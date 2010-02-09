9bb9155d02fe48b4eaedbc60b52b47bc7ade7232
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2010-02-09 08:54:41 +0000

    Added a getHeadPointer() method.

--- trunk/src/library_apps/mathpiper/src/org/mathpiper/lisp/cons/ConsTraverser.java
+++ trunk/src/library_apps/mathpiper/src/org/mathpiper/lisp/cons/ConsTraverser.java
@@ -13,9 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */ //}}}
-
 // :indentSize=4:lineSeparator=\n:noTabs=false:tabSize=4:folding=explicit:collapseFolds=0:
-
 package org.mathpiper.lisp.cons;
 
 import org.mathpiper.lisp.*;
@@ -28,49 +26,53 @@ import org.mathpiper.lisp.cons.Cons;
  * should be used instead of ConsPointer if you are going to traverse
  * a lisp expression in a non-destructive way.
  */
-public class ConsTraverser
-{
-	ConsPointer iPointer;
-        private Environment iEnvironment;
-	
-	public ConsTraverser(Environment aEnvironment, ConsPointer aPtr)
-	{
-            iEnvironment = aEnvironment;
-            iPointer = aPtr;
-	}
+public class ConsTraverser {
 
-     public Object car() throws Exception
-    {
+    ConsPointer iPointer;
+    ConsPointer iHeadPointer;
+
+    private Environment iEnvironment;
+
+    public ConsTraverser(Environment aEnvironment, ConsPointer aPtr) {
+        iEnvironment = aEnvironment;
+        iPointer = aPtr;
+        iHeadPointer = aPtr;
+    }
+
+    public Object car() throws Exception {
         return iPointer.car();
     }
 
-    public ConsPointer cdr()
-    {
+    public ConsPointer cdr() {
         return iPointer.cdr();
     }
-	
-	public Cons getCons()
-	{
-		return iPointer.getCons();
-	}
-	
-	public ConsPointer getPointer()
-	{
-		return iPointer;
-	}
-	
-	public void goNext(int aStackTop) throws Exception
-	{
-		LispError.check(iEnvironment, aStackTop, iPointer.getCons() != null,LispError.NOT_LONG_ENOUGH, "INTERNAL");
-		iPointer = (iPointer.cdr());
-	}
-	
-	public void goSub(int aStackTop) throws Exception
-	{
-		LispError.check(iEnvironment, aStackTop, iPointer.getCons() != null,LispError.INVALID_ARGUMENT, "INTERNAL");
-		LispError.check(iEnvironment, aStackTop, iPointer.car() instanceof ConsPointer,LispError.NOT_A_LIST, "INTERNAL");
-		iPointer = (ConsPointer) iPointer.car();
-	}
 
+    public Cons getCons() {
+        return iPointer.getCons();
+    }
+
+    public void setCons(Cons aCons) {
+        iPointer.setCons(aCons);
+    }
+
+    public ConsPointer getPointer() {
+        return iPointer;
+    }
+
+    public ConsPointer getHeadPointer()
+    {
+        return iHeadPointer;
+    }
+
+    public void goNext(int aStackTop) throws Exception {
+        LispError.check(iEnvironment, aStackTop, iPointer.getCons() != null, LispError.NOT_LONG_ENOUGH, "INTERNAL");
+        iPointer = (iPointer.cdr());
+    }
+
+    public void goSub(int aStackTop) throws Exception {
+        LispError.check(iEnvironment, aStackTop, iPointer.getCons() != null, LispError.INVALID_ARGUMENT, "INTERNAL");
+        LispError.check(iEnvironment, aStackTop, iPointer.car() instanceof ConsPointer, LispError.NOT_A_LIST, "INTERNAL");
+        iPointer = (ConsPointer) iPointer.car();
+    }
 };
 
