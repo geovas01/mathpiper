708ef102f9873d811f369126053ae05256b3854c
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2010-05-29 06:26:01 +0000

    Added documentation for MacroExpand.

--- trunk/src/library_apps/mathpiper/src/org/mathpiper/builtin/functions/optional/MacroExpand.java
+++ trunk/src/library_apps/mathpiper/src/org/mathpiper/builtin/functions/optional/MacroExpand.java
@@ -37,17 +37,66 @@ public class MacroExpand extends BuiltinFunction
                 new BuiltinFunctionEvaluator(this, 1, BuiltinFunctionEvaluator.Fixed | BuiltinFunctionEvaluator.Macro),
                 "MacroExpand");
     }//end method.
+
+
     //todo:tk:this function is not complete yet.  It currently only expands backquoted expressions.
     public void evaluate(Environment aEnvironment, int aStackTop) throws Exception
     {
         org.mathpiper.lisp.behaviours.BackQuoteSubstitute behaviour = new org.mathpiper.lisp.behaviours.BackQuoteSubstitute(aEnvironment);
+
         ConsPointer result = new ConsPointer();
+
         ConsPointer argument = getArgumentPointer(aEnvironment, aStackTop, 1);
+
         Cons argumentCons = argument.getCons();
+
         argument = ((ConsPointer) argumentCons.car()).cdr();
+
         Utility.substitute(aEnvironment, aStackTop, result, argument, behaviour);
+
         String substitutedResult = Utility.printMathPiperExpression(aStackTop, result, aEnvironment, 0);
+
         aEnvironment.write(substitutedResult);
+
         Utility.putTrueInPointer(aEnvironment, getTopOfStackPointer(aEnvironment, aStackTop));
-    }
-}
+
+    }//end method.
+
+}//end class.
+
+
+
+
+/*
+%mathpiper_docs,name="MacroExpand",categories="Programmer Functions;Built In;Programming",access="experimental"
+*CMD MacroExpand --- shows the expanded form of a macro
+*CALL
+    MacroExpand(macro)
+
+*PARMS
+{macro} -- a macro to expand
+
+*DESC
+This function shows the expanded form of the Lisp-like macros that MathPiper supports.
+Note: only back quoted macros are supported at this time.
+
+*E.G.
+//Bind the variable var to the atom Echo.
+In> var := Echo;
+Result: Echo
+
+//Show the macro in expanded form.
+In> MacroExpand(`(@var(2,"Hello")))
+Result: True
+Side Effects:
+Echo(2,"Hello")
+
+//Execute the macro.
+In> `(@var(2,"Hello"))
+Result: True
+Side Effects:
+2 Hello
+
+*SEE `
+%/mathpiper_docs
+*/
