0d33030d88e2e946e7df675e62daad196f87ed63
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2010-06-03 06:44:54 +0000

    Experimenting with drawing simple 3D objects.

--- src/plugins/threedxplormathj_plugin/src/vmm/xm3d/Test.java
+++ src/plugins/threedxplormathj_plugin/src/vmm/xm3d/Test.java
@@ -7,10 +7,24 @@ import vmm.core.Exhibit;
 import vmm.core.UserExhibit;
 import vmm.core.View;
 import vmm.core.UserExhibit.Support;
+import vmm.core3D.Grid3D;
+import vmm.surface.Surface;
 import vmm.surface.parametric.UserSurfaceParametric;
 
 
-public class Test {
+public class Test extends Surface{
+
+
+    
+    protected void createData() {
+            if (data == null || data.getUPatchCount() != uPatchCount.getValue() || data.getVPatchCount() != vPatchCount.getValue())
+                    data = new Grid3D(uPatchCount.getValue(), vPatchCount.getValue());  // create a new grid object only if necessary.
+            int uCount = data.getUCount();
+            int vCount = data.getVCount();
+
+    }//end method.
+
+    
 
     public static void main(String[] args)
     {
