b9ed1768d934053753e0cc5a7a5cefbc0a5c0c38
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2010-06-29 02:08:00 +0000

    Added JLatexMath to the repository and changed ViewLatex, ViewMath, and the documentation so they use it instead of JMathTex.

--- src/library_apps/mathpiper/src/org/mathpiper/ui/gui/help/RenderedLatex.java
+++ src/library_apps/mathpiper/src/org/mathpiper/ui/gui/help/RenderedLatex.java
@@ -1,11 +1,16 @@
 package org.mathpiper.ui.gui.help;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Insets;
 import javax.swing.JLabel;
-import org.mathpiper.ui.gui.jmathtex.TeXFormula;
-import org.mathpiper.ui.gui.jmathtex.TeXIcon;
-import org.mathpiper.ui.gui.jmathtex.exceptions.ParseException;
+import javax.swing.JScrollPane;
+import org.scilab.forge.jlatexmath.TeXConstants;
+import org.scilab.forge.jlatexmath.TeXFormula;
+import org.scilab.forge.jlatexmath.TeXIcon;
+import org.scilab.forge.jlatexmath.DefaultTeXFont;
+import org.scilab.forge.jlatexmath.cyrillic.CyrillicRegistration;
+import org.scilab.forge.jlatexmath.greek.GreekRegistration;
 
 public class RenderedLatex extends JLabel {
 
@@ -18,17 +23,16 @@ public class RenderedLatex extends JLabel {
 
 
     public void setLatex(String latexString) {
-        try {
-            TeXFormula formula = new TeXFormula(latexString);
-            TeXIcon icon = formula.createTeXIcon(0, 17);
-            icon.setInsets(new Insets(1, 1, 1, 1));
+        
+        DefaultTeXFont.registerAlphabet(new CyrillicRegistration());
+	DefaultTeXFont.registerAlphabet(new GreekRegistration());
+	TeXFormula formula = new TeXFormula(latexString);
+	TeXIcon icon = formula.createTeXIcon(TeXConstants.STYLE_DISPLAY, 17);
+	icon.setInsets(new Insets(1, 1, 1, 1));
+        this.setPreferredSize(new Dimension(icon.getIconWidth(), icon.getIconHeight()));
+        this.setAlignmentY(icon.getBaseLine());
+        this.setIcon(icon);
 
-            this.setPreferredSize(new Dimension(icon.getIconWidth(), icon.getIconHeight()));
-            this.setAlignmentY(icon.getBaseLine());
-            this.setIcon(icon);
-        } catch (ParseException pe) {
-            this.setText("Can not render: " + latexString);
-        }
     }
 
 }
