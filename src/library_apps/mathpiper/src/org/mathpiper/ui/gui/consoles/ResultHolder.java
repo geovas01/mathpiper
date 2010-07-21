4ca1468e56258d0c30fcaa4fc5a965e311ebed11
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2010-07-21 22:43:17 +0000

    Font resizing is mostly working now.  Working on resizing the components.

--- src/library_apps/mathpiper/src/org/mathpiper/ui/gui/consoles/ResultHolder.java
+++ src/library_apps/mathpiper/src/org/mathpiper/ui/gui/consoles/ResultHolder.java
@@ -2,22 +2,27 @@ package org.mathpiper.ui.gui.consoles;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.geom.AffineTransform;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import org.scilab.forge.jlatexmath.TeXConstants;
 import org.scilab.forge.jlatexmath.TeXFormula;
 import org.scilab.forge.jlatexmath.TeXIcon;
 
-public class ResultHolder extends JPanel {
+public class ResultHolder extends JPanel implements MouseListener{
 
     private TeXFormula texFormula;
     private JLabel texLabel;
+    private JTextField textResult;
     private String resultString;
+    private int toggle = 0;
+    private JButton toggleButton;
 
 
     public ResultHolder(String latexString, String resultString, int fontPointSize) {
@@ -29,21 +34,31 @@ public class ResultHolder extends JPanel {
         texLabel.setPreferredSize(new Dimension(icon.getIconWidth(), icon.getIconHeight()));
         texLabel.setAlignmentY(icon.getBaseLine());
         texLabel.setIcon(icon);
-        //texLabel.setText(latexString);
 
-        //texLabel.setBackground(Color.white);
-
+        textResult = new JTextField(resultString);
 
 
         this.setBackground(Color.white);
 
         this.setOpaque(true);
 
-        //this.setLayout(new FlowLayout(FlowLayout.LEFT));
-
         this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 
         this.add(texLabel);
+
+
+        toggleButton = new javax.swing.JButton("^");
+        toggleButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent evt) {
+                ResultHolder.this.toggleView();
+            }//end method.
+
+        });
+        toggleButton.setEnabled(true);
+
+
+        this.addMouseListener(this);
 
     }//end constructor.
 
@@ -59,6 +74,58 @@ public class ResultHolder extends JPanel {
         texLabel.repaint();
 
     }//end method.
+
+
+
+
+    void eventOutput(String eventDescription, MouseEvent e) {
+        System.out.println(eventDescription + " detected on "
+                + e.getComponent().getClass().getName()
+                + "." );
+
+    }
+
+    public void mousePressed(MouseEvent e) {
+        //eventOutput("Mouse pressed (# of clicks: " + e.getClickCount() + ")", e);
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        //eventOutput("Mouse released (# of clicks: " + e.getClickCount() + ")", e);
+    }
+
+    public void mouseEntered(MouseEvent e) {
+        //eventOutput("Mouse entered", e);
+    }
+
+    public void mouseExited(MouseEvent e) {
+        //eventOutput("Mouse exited", e);
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        //eventOutput("Mouse clicked (# of clicks: "  + e.getClickCount() + ")", e);
+        toggleView();
+
+    }//end method.
+
+
+    public void toggleView()
+    {
+        this.removeAll();
+
+        if(toggle == 1)
+        {
+            toggle = 0;
+            this.add(texLabel);
+        }
+        else
+        {
+            toggle = 1;
+            this.add(textResult);
+            this.add(toggleButton);
+        }
+
+        this.revalidate();
+    }
 
 
 
