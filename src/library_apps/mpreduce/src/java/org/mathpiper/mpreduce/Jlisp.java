02706d1ca4aa1428b8767464c0dd9e9cb1c3c6c0
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2011-03-28 05:59:39 +0000

    Version bump.

--- src/library_apps/mpreduce/src/java/org/mathpiper/mpreduce/Jlisp.java
+++ src/library_apps/mpreduce/src/java/org/mathpiper/mpreduce/Jlisp.java
@@ -94,7 +94,7 @@ import org.mathpiper.mpreduce.functions.builtin.MPReduceFunctions;
 
 public class Jlisp
 {
-        private static String version = ".07";
+        private static String version = ".08";
 
 	// Within this file I will often reference lispIO and lispErr
 	// directly. Elsewhere they should ONLY be accessed via the Lisp
