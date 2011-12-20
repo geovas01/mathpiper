bec54afa2cf2eb62db01159bce743b2c5956c688
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2011-12-20 19:44:32 +0000

    Beginning the process of transforming MathPiper3 into a non-recursive interpreter. This first step copied the script loading code into LispExpressionEvaluator.

--- branches/mathpiper3/src/org/mathpiper/Version.java
+++ branches/mathpiper3/src/org/mathpiper/Version.java
@@ -22,7 +22,7 @@ package org.mathpiper;
 public class Version
 {
 
-   public static final String version = "js3.003";
+   public static final String version = "js3.004";
 
    public static String version()
    {
