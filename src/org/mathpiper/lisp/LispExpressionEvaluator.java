164f7cd52e70516f422a9aaacb8352339373a610
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2011-12-20 19:44:32 +0000

    Beginning the process of transforming MathPiper3 into a non-recursive interpreter. This first step copied the script loading code into LispExpressionEvaluator.

--- src/org/mathpiper/lisp/LispExpressionEvaluator.java
+++ src/org/mathpiper/lisp/LispExpressionEvaluator.java
@@ -17,11 +17,16 @@
 package org.mathpiper.lisp;
 
 import java.util.Map;
+import org.mathpiper.Scripts;
 
 import org.mathpiper.lisp.cons.Cons;
 import org.mathpiper.builtin.BuiltinFunctionEvaluator;
+import org.mathpiper.io.MathPiperInputStream;
+import org.mathpiper.io.StringInputStream;
+import org.mathpiper.lisp.parsers.MathPiperParser;
 
 import org.mathpiper.lisp.rulebases.SingleArityRulebase;
+import org.mathpiper.lisp.tokenizers.MathPiperTokenizer;
 
 /**
  *  The basic evaluator for Lisp expressions.
@@ -29,43 +34,7 @@ import org.mathpiper.lisp.rulebases.SingleArityRulebase;
  */
 public class LispExpressionEvaluator extends Evaluator {
 
-    /**
-     * <p>
-     * First, the evaluation depth is checked. An error is raised if the maximum evaluation
-     * depth is exceeded.  The next step is the actual evaluation.  aExpression is a
-     * Cons, so we can distinguish three cases:</p>
-     * <ol>
-     * <li value="1"><p>
-     * If  aExpression is a string starting with " , it is
-     * simply copied in aResult. If it starts with another
-     * character (this includes the case where it represents a
-     * number), the environment is checked to see whether a
-     * variable with this name exists. If it does, its value is
-     * copied in aResult, otherwise aExpression is copied.</p>
-     *
-     * <li value="2"><p>
-     * If aExpression is a list, the head of the list is
-     * examined. If the head is not a string. ApplyFast()
-     * is called. If the head is a string, it is checked against
-     * the core commands (if there is a check, the corresponding
-     * evaluator is called). Then it is checked agaist the list of
-     * user function with getRulebase().   Again, the
-     * corresponding evaluator is called if there is a check. If
-     * all fails, ReturnUnEvaluated() is called.</p>
-     * <li value="3"><p>
-     * Otherwise (ie. if aExpression is a getJavaObject object), it is
-     * copied in aResult.</p>
-     * </ol>
-     *
-     * <p>
-     * Note: The result of this operation must be a unique (copied)
-     * element! Eg. its Next might be set...</p>
-     *
-     * @param aEnvironment  the Lisp environment, in which the evaluation should take place
-     * @param aResult             the result of the evaluation
-     * @param aExpression     the expression to evaluate
-     * @throws java.lang.Exception
-     */
+
     public Cons evaluate(Environment aEnvironment, int aStackTop, Cons aExpression) throws Exception {
 
         if (aExpression == null) {
@@ -78,7 +47,7 @@ public class LispExpressionEvaluator extends Evaluator {
             if (aEnvironment.iEvalDepth >= aEnvironment.iMaxEvalDepth) {
                 LispError.throwError(aEnvironment, aStackTop, LispError.MAXIMUM_RECURSE_DEPTH_REACHED, "Maximum recursed depth set to " + aEnvironment.iMaxEvalDepth + ".");
             }
-            // }
+
         }
 
 
@@ -118,18 +87,6 @@ public class LispExpressionEvaluator extends Evaluator {
             }
 
 
-            /*
-            Map metaDataMap = aExpression.getCons().getMetadataMap();
-            int lineNumber = aEnvironment.iCurrentInput.iStatus.getLineNumber();
-            int startIndex = -1;
-            int endIndex = aEnvironment.iCurrentInput.iStatus.getLineIndex();
-            if (metaDataMap != null) {
-            lineNumber = (Integer) metaDataMap.get("lineNumber");
-            startIndex = (Integer) metaDataMap.get("startIndex");
-            endIndex = (Integer) metaDataMap.get("endIndex");
-            }
-            LispError.raiseError("Problem with atom ***(" + atomName + ")***, <wrong code: " + Utility.printLispExpression(-1, aExpression, aEnvironment, 50) + ">,", "[Internal]", lineNumber, startIndex, endIndex, aStackTop, aEnvironment);
-             */
 
             //Handle unbound variables.
 
@@ -159,9 +116,98 @@ public class LispExpressionEvaluator extends Evaluator {
                         return builtinInFunctionEvaluator.evaluate(aEnvironment, aStackTop, functionAndArgumentsList);
                     }
 
+
                     //User function handler.
-                    SingleArityRulebase userFunction;
-                    userFunction = getUserFunction(aEnvironment, aStackTop, functionAndArgumentsList);
+                    Cons head2 = functionAndArgumentsList;
+
+                    if (!(head2.car() instanceof String)) {
+                        LispError.throwError(aEnvironment, aStackTop, "No function name specified.");
+                    }
+
+                    SingleArityRulebase userFunction = (SingleArityRulebase) aEnvironment.getRulebase(aStackTop, functionAndArgumentsList);
+
+
+                    if (userFunction == null) {
+
+                        //=============== Load library function.
+                        Scripts scripts = aEnvironment.scripts;
+
+                        String[] scriptCode = scripts.getScript(functionName);
+
+
+                        if (scriptCode == null) {
+                            LispError.throwError(aEnvironment, aStackTop, "No script returned for function: " + functionName + " from Scripts.java.");
+                        }
+
+                        if (scriptCode[0] == null) {
+
+                            if (scriptCode[1] == null) {
+                                LispError.throwError(aEnvironment, aStackTop, "No script returned for function: " + functionName + " from Scripts.java.");
+                            }
+
+
+
+                            String scriptString = scriptCode[1];
+
+
+
+                            StringInputStream functionInputStream = new StringInputStream(scriptString, aEnvironment.getCurrentInput().iStatus);
+
+                            scriptCode[0] = "+";
+
+                            // ============= Do internal load.
+                            MathPiperInputStream previous = aEnvironment.getCurrentInput();
+                            try {
+                                aEnvironment.setCurrentInput(functionInputStream);
+
+                                String eof = "EndOfFile";
+                                boolean endoffile = false;
+
+                                MathPiperParser parser = new MathPiperParser(new MathPiperTokenizer(), aEnvironment.getCurrentInput(), aEnvironment, aEnvironment.iPrefixOperators, aEnvironment.iInfixOperators, aEnvironment.iPostfixOperators, aEnvironment.iBodiedOperators);
+                                //Parser parser = new Parser(new MathPiperTokenizer(), aEnvironment.iCurrentInput, aEnvironment);
+
+                                while (!endoffile) {
+                                    // Read expression
+                                    Cons readIn = parser.parse(aStackTop);
+
+                                    if (readIn == null) {
+                                        LispError.throwError(aEnvironment, aStackTop, LispError.READING_FILE, "");
+                                    }
+                                    // check for end of file
+                                    if (readIn.car() instanceof String && ((String) readIn.car()).equals(eof)) {
+                                        endoffile = true;
+                                    } // Else evaluate
+                                    else {
+
+                                        Cons result = aEnvironment.iLispExpressionEvaluator.evaluate(aEnvironment, aStackTop, readIn);
+                                        if (aStackTop != -1) {
+                                            aEnvironment.setLocalOrGlobalVariable(aStackTop, "$LoadResult", result, false);//Note:tk:added to make the result of executing Loaded code available.
+                                        }
+                                    }
+                                }//end while.
+
+
+
+                            } catch (Exception e) {
+                                //System.out.println(e.getMessage()); e.printStackTrace(); //todo:tk:uncomment for debugging.
+
+                                throw e;
+                            } finally {
+                                aEnvironment.setCurrentInput(previous);
+                            }
+
+                            // ============ end do internal load.
+
+
+                        }
+
+                        //=============== end Load library function
+
+                        userFunction = (SingleArityRulebase) aEnvironment.getRulebase(aStackTop, functionAndArgumentsList);
+
+                    }
+
+
                     if (userFunction != null) {
 
                         aEnvironment.iEvalDepth--;
@@ -199,24 +245,7 @@ public class LispExpressionEvaluator extends Evaluator {
                     aEnvironment.iEvalDepth--;
                     return Utility.applyPure(aStackTop, operator, args2, aEnvironment);
                 }
-                //printf("**** Undef: %s\n",head.String().String());
 
-
-                /*  todo:tk: This code is for experimenting with having non-existent functions throw an exception when they are called.
-                if (functionName.equals("_")) {
-                Utility.returnUnEvaluated(aStackTop, aResult, subList, aEnvironment);
-                aEnvironment.iEvalDepth--;
-                return;
-                } else {
-                LispError.raiseError("The function " + functionName + " is not defined.\n", null, aStackTop, aEnvironment );
-                }*/
-
-
-                //Utility.returnUnEvaluated(aStackTop, aResult, subList, aEnvironment);
-
-                //aEnvironment.iEvalDepth--;
-
-                //return;
 
             }
         }
@@ -226,268 +255,5 @@ public class LispExpressionEvaluator extends Evaluator {
 
         return aExpression.copy(false);
     }
-
-    SingleArityRulebase getUserFunction(Environment aEnvironment, int aStackTop, Cons subList) throws Exception {
-        Cons head = subList;
-
-        if (!(head.car() instanceof String)) {
-            LispError.throwError(aEnvironment, aStackTop, "No function name specified.");
-        }
-
-        String functionName = (String) head.car();
-
-
-
-        SingleArityRulebase userFunc = (SingleArityRulebase) aEnvironment.getRulebase(aStackTop, subList);
-
-        //if (userFunc == null && functionName.equals("f")) {
-        //    int xx = 1;
-        //}
-
-
-        if (userFunc != null) {
-            return userFunc;
-
-        } else {
-
-
-            //System.out.println(functionName);
-
-            Utility.loadLibraryFunction(functionName, aEnvironment, aStackTop);
-
-            userFunc = (SingleArityRulebase) aEnvironment.getRulebase(aStackTop, subList);
-
-        }
-
-
-        return userFunc;
-
-    }//end method.
-    /*
-    void TracedStackEvaluator::PushFrame()
-    {
-    UserStackInformation *op = NEW UserStackInformation;
-    objs.Append(op);
-    }
-
-    void TracedStackEvaluator::PopFrame()
-    {
-    LISPASSERT (objs.Size() > 0);
-    if (objs[objs.Size()-1])
-    {
-    delete objs[objs.Size()-1];
-    objs[objs.Size()-1] = null;
-    }
-    objs.Delete(objs.Size()-1);
-    }
-
-    void TracedStackEvaluator::ResetStack()
-    {
-    while (objs.Size()>0)
-    {
-    PopFrame();
-    }
-    }
-
-    UserStackInformation& TracedStackEvaluator::StackInformation()
-    {
-    return *(objs[objs.Size()-1]);
-    }
-
-    TracedStackEvaluator::~TracedStackEvaluator()
-    {
-    ResetStack();
-    }
-
-    void TracedStackEvaluator::ShowStack(Environment aEnvironment, LispOutput& aOutput)
-    {
-    LispLocalEvaluator local(aEnvironment,NEW BasicEvaluator);
-
-    LispInt i;
-    LispInt from=0;
-    LispInt upto = objs.Size();
-
-    for (i=from;i<upto;i++)
-    {
-    LispChar str[20];
-    #ifdef YACAS_DEBUG
-    aEnvironment.write(objs[i].iFileName);
-    aEnvironment.write("(");
-    InternalIntToAscii(str,objs[i].iLine);
-    aEnvironment.write(str);
-    aEnvironment.write(") : ");
-    aEnvironment.write("Debug> ");
-    #endif
-    InternalIntToAscii(str,i);
-    aEnvironment.write(str);
-    aEnvironment.write(": ");
-    aEnvironment.CurrentPrinter().Print(objs[i].iOperator, *aEnvironment.CurrentOutput(),aEnvironment);
-
-    LispInt internal;
-    internal = (null != aEnvironment.CoreCommands().LookUp(objs[i].iOperator.String()));
-    if (internal)
-    {
-    aEnvironment.write(" (Internal function) ");
-    }
-    else
-    {
-    if (objs[i].iRulePrecedence>=0)
-    {
-    aEnvironment.write(" (Rule # ");
-    InternalIntToAscii(str,objs[i].iRulePrecedence);
-    aEnvironment.write(str);
-    if (objs[i].iSide)
-    aEnvironment.write(" in body) ");
-    else
-    aEnvironment.write(" in pattern) ");
-    }
-    else
-    aEnvironment.write(" (User function) ");
-    }
-    if (!!objs[i].iExpression)
-    {
-    aEnvironment.write("\n      ");
-    if (aEnvironment.iEvalDepth>(aEnvironment.iMaxEvalDepth-10))
-    {
-    LispString expr;
-    PrintExpression(expr, objs[i].iExpression,aEnvironment,60);
-    aEnvironment.write(expr.c_str());
-    }
-    else
-    {
-    LispPtr getSubList = objs[i].iExpression.SubList();
-    if (!!getSubList && !!getSubList)
-    {
-    LispString expr;
-    LispPtr out(objs[i].iExpression);
-    PrintExpression(expr, out,aEnvironment,60);
-    aEnvironment.write(expr.c_str());
-    }
-    }
-    }
-    aEnvironment.write("\n");
-    }
-    }
-
-    void TracedStackEvaluator::Eval(Environment aEnvironment, ConsPointer aResult,
-    ConsPointer aExpression)
-    {
-    if (aEnvironment.iEvalDepth>=aEnvironment.iMaxEvalDepth)
-    {
-    ShowStack(aEnvironment, *aEnvironment.CurrentOutput());
-    CHK2(aEnvironment.iEvalDepth<aEnvironment.iMaxEvalDepth,
-    MAXIMUM_RECURSE_DEPTH_REACHED);
-    }
-
-    LispPtr getSubList = aExpression.SubList();
-    LispString * str = null;
-    if (getSubList)
-    {
-    Cons head = getSubList;
-    if (head)
-    {
-    str = head.String();
-    if (str)
-    {
-    PushFrame();
-    UserStackInformation& st = StackInformation();
-    st.iOperator = (LispAtom::New(aEnvironment,str.c_str()));
-    st.iExpression = (aExpression);
-    #ifdef YACAS_DEBUG
-    if (aExpression.iFileName)
-    {
-    st.iFileName = aExpression.iFileName;
-    st.iLine = aExpression.iLine;
-    }
-    #endif
-    }
-    }
-    }
-    BasicEvaluator::Eval(aEnvironment, aResult, aExpression);
-    if (str)
-    {
-    PopFrame();
-    }
-    }
-
-    void TracedEvaluator::Eval(Environment aEnvironment, ConsPointer aResult,
-    ConsPointer aExpression)
-    {
-    if(!aEnvironment.iDebugger) RaiseError("Internal error: debugging failing");
-    if(aEnvironment.iDebugger.Stopped()) RaiseError("");
-
-    REENTER:
-    errorStr.ResizeTo(1); errorStr[0] = '\0';
-    LispTrap(aEnvironment.iDebugger.Enter(aEnvironment, aExpression),errorOutput,aEnvironment);
-    if(aEnvironment.iDebugger.Stopped()) RaiseError("");
-    if (errorStr[0])
-    {
-    aEnvironment.write(errorStr.c_str());
-    aEnvironment.iEvalDepth=0;
-    goto REENTER;
-    }
-
-    errorStr.ResizeTo(1); errorStr[0] = '\0';
-    LispTrap(BasicEvaluator::Eval(aEnvironment, aResult, aExpression),errorOutput,aEnvironment);
-
-    if (errorStr[0])
-    {
-    aEnvironment.write(errorStr.c_str());
-    aEnvironment.iEvalDepth=0;
-    aEnvironment.iDebugger.Error(aEnvironment);
-    goto REENTER;
-    }
-
-    if(aEnvironment.iDebugger.Stopped()) RaiseError("");
-
-    aEnvironment.iDebugger.Leave(aEnvironment, aResult, aExpression);
-    if(aEnvironment.iDebugger.Stopped()) RaiseError("");
-    }
-
-    YacasDebuggerBase::~YacasDebuggerBase()
-    {
-    }
-
-    void DefaultDebugger::Start()
-    {
-    }
-
-    void DefaultDebugger::Finish()
-    {
-    }
-
-    void DefaultDebugger::Enter(Environment aEnvironment,
-    ConsPointer aExpression)
-    {
-    LispLocalEvaluator local(aEnvironment,NEW BasicEvaluator);
-    iTopExpr = (aExpression.Copy());
-    LispPtr result;
-    defaultEval.Eval(aEnvironment, result, iEnter);
-    }
-
-    void DefaultDebugger::Leave(Environment aEnvironment, ConsPointer aResult,
-    ConsPointer aExpression)
-    {
-    LispLocalEvaluator local(aEnvironment,NEW BasicEvaluator);
-    LispPtr result;
-    iTopExpr = (aExpression.Copy());
-    iTopResult = (aResult);
-    defaultEval.Eval(aEnvironment, result, iLeave);
-    }
-
-    LispBoolean DefaultDebugger::Stopped()
-    {
-    return iStopped;
-    }
-
-    void DefaultDebugger::Error(Environment aEnvironment)
-    {
-    LispLocalEvaluator local(aEnvironment,NEW BasicEvaluator);
-    LispPtr result;
-    defaultEval.Eval(aEnvironment, result, iException);
-    }
-
-
-     */
 }//end class.
 
