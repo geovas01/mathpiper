2e6489d8f4f27cf827009afd6ba8719e0cad8267
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2012-02-28 21:24:31 +0000

    Renamed => to Implies?.

--- src/library_apps/mathpiper2/src/org/mathpiper/Version.java
+++ src/library_apps/mathpiper2/src/org/mathpiper/Version.java
@@ -22,7 +22,7 @@ package org.mathpiper;
 public class Version
 {
 
-   public static final String version = "js.081";
+   public static final String version = "js.082";
 
    public static String version()
    {
