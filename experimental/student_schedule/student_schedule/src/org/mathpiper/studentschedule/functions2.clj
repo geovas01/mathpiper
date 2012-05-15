1add9c22ccefdb80cd4e1706bcb27e9c215a065f
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2012-05-15 07:52:13 +0000

    Started adjusting the HTML code so less rows are displayed.

--- experimental/student_schedule/student_schedule/src/org/mathpiper/studentschedule/functions2.clj
+++ experimental/student_schedule/student_schedule/src/org/mathpiper/studentschedule/functions2.clj
@@ -1,57 +1,57 @@
 
 
 
-(defn leastDays [schedule]
-
-  
-  (every?
-  (fn [class] (every? 
-                (fn [timecode]
-                  (= (timecode 0) day) ) (class 2)) ) schedule))
-
-
-(filter #(query  %) legalSchedules)
-
-
-
-
-;;Count the number of bits that are set in an interger.
-(defn bit-count1 [n]
-  (count
-    (take-while
-      #(not (zero? %))
-      (iterate #(bit-and % (dec %)) n))))
-
-
-;;Count the number of bits that are set in an integer.
-(defn bit-count2 [v]
-  (loop [v v
-         c 0]
-    (if (zero? v)
-        c
-        (recur (bit-and v (dec v)) (inc c)))))
-
-
-
-;;Count how many days a course is spread over.
-(defn courseDayCount [[_ _ daysAndTimes]]
-  
-  (apply max (map (fn [dayTime] (bit-count2 (dayTime 0))) daysAndTimes))
-  
-  )
-
-
-
-;;Sort a legal schedules list by the number of days it is over.
-(defn leastDays [schedule]
- 
-       (sort (fn [[course1Number course1Section course1DaysAndTimes] [course2Number course2Section course2DaysAndTimes]] (< (courseDayCount course1DaysAndTimes) (courseDayCount course2DaysAndTimes))) schedule)
-       
-       )
-       
-       
-
-
+; (defn leastDays [schedule]
+; 
+;   
+;   (every?
+;   (fn [class] (every? 
+;                 (fn [timecode]
+;                   (= (timecode 0) day) ) (class 2)) ) schedule))
+; 
+; 
+; (filter #(query  %) legalSchedules)
+; 
+; 
+; 
+; 
+; ;;Count the number of bits that are set in an interger.
+; (defn bit-count1 [n]
+;   (count
+;     (take-while
+;       #(not (zero? %))
+;       (iterate #(bit-and % (dec %)) n))))
+; 
+; 
+; ;;Count the number of bits that are set in an integer.
+; (defn bit-count2 [v]
+;   (loop [v v
+;          c 0]
+;     (if (zero? v)
+;         c
+;         (recur (bit-and v (dec v)) (inc c)))))
+; 
+; 
+; 
+; ;;Count how many days a course is spread over.
+; (defn courseDayCount [[_ _ daysAndTimes]]
+;   
+;   (apply max (map (fn [dayTime] (bit-count2 (dayTime 0))) daysAndTimes))
+;   
+;   )
+; 
+; 
+; 
+; ;;Sort a legal schedules list by the number of days it is over.
+; (defn leastDays [schedule]
+;  
+;        (sort (fn [[course1Number course1Section course1DaysAndTimes] [course2Number course2Section course2DaysAndTimes]] (< (courseDayCount course1DaysAndTimes) (courseDayCount course2DaysAndTimes))) schedule)
+;        
+;        )
+;        
+;        
+; 
+; 
 
 ;Change section format so each section has only one time code.
 ;
@@ -120,66 +120,66 @@
 
 ;;Add open time blocks.
 ;
-(def weekComplete [])
-
-(doseq [day week]
-(do   
-
-  ;(vec (cons courseNumber section))
-    (def dayEnd (conj day [:end :x [0,289,0]] ))
-    (def dayOpen [])
-    
-    (def indexTime 0)
-    (def index 0)
-    (while (< indexTime 288)
-    (do   
-        (def nextTime (((dayEnd index) 2) 1))
-        (def timeInc (((dayEnd index) 2 ) 2))
-
-        (if (not= indexTime nextTime) (def dayOpen (conj dayOpen [:Open :x [0 indexTime (- nextTime indexTime)]])))
-       
-        (def indexTime (+ nextTime timeInc))
- 
-        (def index (inc index))
-    ))
-
-    (def dayComplete (vec (sort (fn [x,y] (< ((x 2) 1) ((y 2) 1))) (vec (concat day dayOpen)))))
-
-    (def weekComplete (conj weekComplete dayComplete))
-
-))
-
-
-;Before opens are added.
-[[:MATH1300 :01 [80 120 22]] [:ARTH1101 :02 [80 144 15]] [:end :x [0 289 0]]]
-[[:ETEM1110 :01 [40 96 22]] [:ETCO1120 :51 [32 204 58]] [:end :x [0 289 0]]]
-[[:MATH1300 :01 [80 120 22]] [:ARTH1101 :02 [80 144 15]] [:end :x [0 289 0]]]
-[[:ETEM1110 :01 [40 96 22]] [:end :x [0 289 0]]]
-[[:MATH1300 :01 [4 120 10]] [:end :x [0 289 0]]]
-[[:end :x [0 289 0]]]
-[[:end :x [0 289 0]]]
-
-
-;After opens are added.
-[
-[[:Open :x [0 0 120]] [:MATH1300 :01 [80 120 22]] [:Open :x [0 142 2]] [:ARTH1101 :02 [80 144 15]] [:Open :x [0 159 130]]]
-[[:Open :x [0 0 96]] [:ETEM1110 :01 [40 96 22]] [:Open :x [0 118 86]] [:ETCO1120 :51 [32 204 58]] [:Open :x [0 262 27]]]
-[[:Open :x [0 0 120]] [:MATH1300 :01 [80 120 22]] [:Open :x [0 142 2]] [:ARTH1101 :02 [80 144 15]] [:Open :x [0 159 130]]]
-[[:Open :x [0 0 96]] [:ETEM1110 :01 [40 96 22]] [:Open :x [0 118 171]]]
-[[:Open :x [0 0 120]] [:MATH1300 :01 [4 120 10]] [:Open :x [0 130 159]]] 
-[[:Open :x [0 0 289]]] 
-[[:Open :x [0 0 289]]]
-]
-
-[
-[[:Open :x [0 0 120]] [:MATH1300 :01 [80 120 22]] [:Open :x [0 142 2]] [:ARTH1101 :02 [80 144 15]] [:Open :x [0 159 130]]]
-[[:Open :x [0 0 96]] [:ETEM1110 :01 [40 96 22]] [:Open :x [0 118 86]] [:ETCO1120 :51 [32 204 58]] [:Open :x [0 262 27]]]
-[[:Open :x [0 0 120]] [:MATH1300 :01 [80 120 22]] [:Open :x [0 142 2]] [:ARTH1101 :02 [80 144 15]] [:Open :x [0 159 130]]]
-[[:Open :x [0 0 96]] [:ETEM1110 :01 [40 96 22]] [:Open :x [0 118 171]]] 
-[[:Open :x [0 0 120]] [:MATH1300 :01 [4 120 10]][:Open :x [0 130 159]]] 
-[[:Open :x [0 0 289]]] 
-[[:Open :x [0 0 289]]]]
-
+; (def weekComplete [])
+; 
+; (doseq [day week]
+; (do   
+; 
+;   ;(vec (cons courseNumber section))
+;     (def dayEnd (conj day [:end :x [0,289,0]] ))
+;     (def dayOpen [])
+;     
+;     (def indexTime 0)
+;     (def index 0)
+;     (while (< indexTime 288)
+;     (do   
+;         (def nextTime (((dayEnd index) 2) 1))
+;         (def timeInc (((dayEnd index) 2 ) 2))
+; 
+;         (if (not= indexTime nextTime) (def dayOpen (conj dayOpen [:Open :x [0 indexTime (- nextTime indexTime)]])))
+;        
+;         (def indexTime (+ nextTime timeInc))
+;  
+;         (def index (inc index))
+;     ))
+; 
+;     (def dayComplete (vec (sort (fn [x,y] (< ((x 2) 1) ((y 2) 1))) (vec (concat day dayOpen)))))
+; 
+;     (def weekComplete (conj weekComplete dayComplete))
+; 
+; ))
+; 
+; 
+; ;Before opens are added.
+; [[:MATH1300 :01 [80 120 22]] [:ARTH1101 :02 [80 144 15]] [:end :x [0 289 0]]]
+; [[:ETEM1110 :01 [40 96 22]] [:ETCO1120 :51 [32 204 58]] [:end :x [0 289 0]]]
+; [[:MATH1300 :01 [80 120 22]] [:ARTH1101 :02 [80 144 15]] [:end :x [0 289 0]]]
+; [[:ETEM1110 :01 [40 96 22]] [:end :x [0 289 0]]]
+; [[:MATH1300 :01 [4 120 10]] [:end :x [0 289 0]]]
+; [[:end :x [0 289 0]]]
+; [[:end :x [0 289 0]]]
+; 
+; 
+; ;After opens are added.
+; [
+; [[:Open :x [0 0 120]] [:MATH1300 :01 [80 120 22]] [:Open :x [0 142 2]] [:ARTH1101 :02 [80 144 15]] [:Open :x [0 159 130]]]
+; [[:Open :x [0 0 96]] [:ETEM1110 :01 [40 96 22]] [:Open :x [0 118 86]] [:ETCO1120 :51 [32 204 58]] [:Open :x [0 262 27]]]
+; [[:Open :x [0 0 120]] [:MATH1300 :01 [80 120 22]] [:Open :x [0 142 2]] [:ARTH1101 :02 [80 144 15]] [:Open :x [0 159 130]]]
+; [[:Open :x [0 0 96]] [:ETEM1110 :01 [40 96 22]] [:Open :x [0 118 171]]]
+; [[:Open :x [0 0 120]] [:MATH1300 :01 [4 120 10]] [:Open :x [0 130 159]]] 
+; [[:Open :x [0 0 289]]] 
+; [[:Open :x [0 0 289]]]
+; ]
+; 
+; [
+; [[:Open :x [0 0 120]] [:MATH1300 :01 [80 120 22]] [:Open :x [0 142 2]] [:ARTH1101 :02 [80 144 15]] [:Open :x [0 159 130]]]
+; [[:Open :x [0 0 96]] [:ETEM1110 :01 [40 96 22]] [:Open :x [0 118 86]] [:ETCO1120 :51 [32 204 58]] [:Open :x [0 262 27]]]
+; [[:Open :x [0 0 120]] [:MATH1300 :01 [80 120 22]] [:Open :x [0 142 2]] [:ARTH1101 :02 [80 144 15]] [:Open :x [0 159 130]]]
+; [[:Open :x [0 0 96]] [:ETEM1110 :01 [40 96 22]] [:Open :x [0 118 171]]] 
+; [[:Open :x [0 0 120]] [:MATH1300 :01 [4 120 10]][:Open :x [0 130 159]]] 
+; [[:Open :x [0 0 289]]] 
+; [[:Open :x [0 0 289]]]]
+; 
 ;weekComplete
 ;64 32 16  8  4 2 1
 ; M  T  W  R  F
@@ -190,7 +190,7 @@
 
 (defn addOpenTimeBlocks [schedule]
   
-  (println "Hello")
+
   (let [week (orderIntoDays schedule)]
     
     (vec (for [day week]
@@ -278,11 +278,11 @@
 </tr>
 "  
       
-    (apply str (apply concat (for [row (range 0 288)]
+    (apply str (apply concat (for [row (range 96 288)]
       (apply concat
              
         
-        (conj accumulator (str "<tr> <th nowrap>" row "</th>"))
+        (conj accumulator (str "<tr> " (if (= (mod row 6) 0) (str "<th rowspan= 6 nowrap>" row "</th>"))))
         
         
     (apply str (apply concat (for [ day days, [courseNumber sectionNumber [daysCode startTime duration]] day]
@@ -293,7 +293,7 @@
         (if (= startTime row)
           (do
                   
-                  (conj accumulator (str "<td align=center rowspan=" duration " >" (name courseNumber) " " (name sectionNumber) "</td>"))
+                  (conj accumulator (str "<td align=center rowspan=" duration " >" (if (= courseNumber :Open) (str "<font color=blue>" (name courseNumber) " </font>") (name courseNumber)) " " (if (= sectionNumber :x) "" (name sectionNumber)) "</td>"))
 
                   )
           )
@@ -317,7 +317,7 @@
 </body>
 </html>"    
     
-    
+    (count legalSchedules)(
     )
     
     
@@ -325,11 +325,11 @@
     
     )
   
-  )
+  ))
 
 
 
-
+;(spit "../student_schedule.html" (createHtml (nth legalSchedules 0)))
 
 
 
--- experimental/student_schedule/student_schedule/src/org/mathpiper/studentschedule/unidiomatic_schedule_engine.clj
+++ experimental/student_schedule/student_schedule/src/org/mathpiper/studentschedule/unidiomatic_schedule_engine.clj
@@ -1,6 +1,6 @@
 
 ;;Determine possible course combinations. [:ENGL1101]
-(def courses [[:ETCO1120] [:ETEM1110] [:MATH1300] [:ARTH1101 :ENGL2275 :MUSI1201 :MUSI2211 :PHIL3300 :THAR1000]])
+(def courses [[:ETCO1120] [:ETEM1110] [:MATH1300] [:ENGL1101] [:ARTH1101 :ENGL2275 :MUSI1201 :MUSI2211 :PHIL3300 :THAR1000]])
 
 (def possibleCourses [[]])
 (def possibleCoursesRevised [])
@@ -124,164 +124,168 @@
 
 
 
-;;Prepares student schedule.
 
-;Change section format so each section has only one time code.
-(def scheduleA (nth legalSchedules 0))
-
-(def schedulesTimeSlots [])
-  
-(doseq [section scheduleA]
-  
-    (doseq [sectionTime (section 2)]
-    
-        (def schedulesTimeSlots (conj  schedulesTimeSlots [(section 0) (section 1) sectionTime]))
-    )
-)
-
-
-
-;Orders previously formatted single timecode sections into days of a week.
-(def week [])
-(doseq [daybit [64 32 16 8 4 2 1]]
-(do 
-    (def day [])
-    
-    (doseq [section schedulesTimeSlots]
-      (if (not= (bit-and ((section 2) 0) daybit) 0) 
-        (do
-            (def day (conj day section))
-            )
-      ))
-
-     (def day (vec (sort (fn [x y] (< ((x 2) 1) ((y 2) 1))) day) ))
-    
-     (def week (conj week day))
-  )
-    
-)
-    
-
-;(prn week)
-
-
-
-
-
-;;Add open time blocks.
-(def weekComplete [])
-
-(doseq [day week]
-(do   
-
-  ;(vec (cons courseNumber section))
-    (def dayEnd (conj day [:end :x [0,289,0]] ))
-  
-  ;(prn dayEnd)
-  
-    (def dayOpen [])
-    
-    (def indexTime 0)
-    (def index 0)
-    (while (< indexTime 288)
-    (do   
-      
-        (def nextTime (((dayEnd index) 2) 1))
-        (def timeInc (((dayEnd index) 2 ) 2))
-        
-      (println index indexTime nextTime timeInc dayEnd)
-
-        (if (not= indexTime nextTime) (def dayOpen (conj dayOpen [:Open :x [0 indexTime (- nextTime indexTime)]])))
-       
-        (def indexTime (+ nextTime timeInc))
- 
-        (def index (inc index))
-    ))
-
-    (def dayComplete (vec (sort (fn [x,y] (< ((x 2) 1) ((y 2) 1))) (vec (concat day dayOpen)))))
-
-    (def weekComplete (conj weekComplete dayComplete))
-
-))
-;weekComplete
-
-
-
-
-;;Generate html.
-
-(def htmlRows "")
-
-
-(def row 0)
-
-(while (< row 288)
-(do
-    (def htmlTR (str "<tr> <th nowrap>" row "</th>"))
-    
-    (def htmlTD "")
-    (doseq [day weekComplete]
-    
-   ;(println "B " row)  
-        (doseq [section day]
-        (do
-          ;(println "C " row) 
-            (def courseNumber (section 0))
-            (def sectionNumber (section 1))
-            
-            (def daysAndTime (section 2))
-            
-
-                (def daysCode (daysAndTime 0))
-
-                ;(println "D " daysAndTime (daysAndTime 1) row) 
-                
-                (if (= (daysAndTime 1) row)
-                (do
-                   
-                  (def htmlTD (str htmlTD "<td align=center rowspan=" (daysAndTime 2) " >" (name courseNumber) " " (name sectionNumber) "</td>"))
-                   ;(println "E " row htmlTD)
-                  )
-                )
-
- 
-            
-        
-        )))
-            
-   ;(println "G " row) 
-    
-    (def htmlTR (str htmlTR htmlTD "</tr>" "\n" ))
-    
-    (def htmlRows (str htmlRows htmlTR))
-
-    (def row (inc row))
-))
-
-
-(def htmlTop "
-<html>
-<body>
-<table border=1 width=100%>
-<tr>
-<td nowrap></td>
-<th width=16%>Monday</th>
-<th width=16%>Tuesday</th>
-<th width=16%>Wednesday</th>
-<th width=16%>Thursday</th>
-<th width=16%>Friday</th>
-</tr>
-")
-
-
-
-(def html (str htmlTop htmlRows 
-"</table>
-</body>
-</html>"))
-
-(println "Done")
-
-
-(spit "../student_schedule.html" html)
-
+;  
+;  
+;  ;;Prepares student schedule.
+;  
+;  ;Change section format so each section has only one time code.
+;  (def scheduleA (nth legalSchedules 0))
+;  
+;  (def schedulesTimeSlots [])
+;    
+;  (doseq [section scheduleA]
+;    
+;      (doseq [sectionTime (section 2)]
+;      
+;          (def schedulesTimeSlots (conj  schedulesTimeSlots [(section 0) (section 1) sectionTime]))
+;      )
+;  )
+;  
+;  
+;  
+;  ;Orders previously formatted single timecode sections into days of a week.
+;  (def week [])
+;  (doseq [daybit [64 32 16 8 4 2 1]]
+;  (do 
+;      (def day [])
+;      
+;      (doseq [section schedulesTimeSlots]
+;        (if (not= (bit-and ((section 2) 0) daybit) 0) 
+;          (do
+;              (def day (conj day section))
+;              )
+;        ))
+;  
+;       (def day (vec (sort (fn [x y] (< ((x 2) 1) ((y 2) 1))) day) ))
+;      
+;       (def week (conj week day))
+;    )
+;      
+;  )
+;      
+;  
+;  ;(prn week)
+;  
+;  
+;  
+;  
+;  
+;  ;;Add open time blocks.
+;  (def weekComplete [])
+;  
+;  (doseq [day week]
+;  (do   
+;  
+;    ;(vec (cons courseNumber section))
+;      (def dayEnd (conj day [:end :x [0,289,0]] ))
+;    
+;    ;(prn dayEnd)
+;    
+;      (def dayOpen [])
+;      
+;      (def indexTime 0)
+;      (def index 0)
+;      (while (< indexTime 288)
+;      (do   
+;        
+;          (def nextTime (((dayEnd index) 2) 1))
+;          (def timeInc (((dayEnd index) 2 ) 2))
+;          
+;        (println index indexTime nextTime timeInc dayEnd)
+;  
+;          (if (not= indexTime nextTime) (def dayOpen (conj dayOpen [:Open :x [0 indexTime (- nextTime indexTime)]])))
+;         
+;          (def indexTime (+ nextTime timeInc))
+;   
+;          (def index (inc index))
+;      ))
+;  
+;      (def dayComplete (vec (sort (fn [x,y] (< ((x 2) 1) ((y 2) 1))) (vec (concat day dayOpen)))))
+;  
+;      (def weekComplete (conj weekComplete dayComplete))
+;  
+;  ))
+;  ;weekComplete
+;  
+;  
+;  
+;  
+;  ;;Generate html.
+;  
+;  (def htmlRows "")
+;  
+;  
+;  (def row 0)
+;  
+;  (while (< row 288)
+;  (do
+;      (def htmlTR (str "<tr> <th nowrap>" row "</th>"))
+;      
+;      (def htmlTD "")
+;      (doseq [day weekComplete]
+;      
+;     ;(println "B " row)  
+;          (doseq [section day]
+;          (do
+;            ;(println "C " row) 
+;              (def courseNumber (section 0))
+;              (def sectionNumber (section 1))
+;              
+;              (def daysAndTime (section 2))
+;              
+;  
+;                  (def daysCode (daysAndTime 0))
+;  
+;                  ;(println "D " daysAndTime (daysAndTime 1) row) 
+;                  
+;                  (if (= (daysAndTime 1) row)
+;                  (do
+;                     
+;                    (def htmlTD (str htmlTD "<td align=center rowspan=" (daysAndTime 2) " >" (name courseNumber) " " (name sectionNumber) "</td>"))
+;                     ;(println "E " row htmlTD)
+;                    )
+;                  )
+;  
+;   
+;              
+;          
+;          )))
+;              
+;     ;(println "G " row) 
+;      
+;      (def htmlTR (str htmlTR htmlTD "</tr>" "\n" ))
+;      
+;      (def htmlRows (str htmlRows htmlTR))
+;  
+;      (def row (inc row))
+;  ))
+;  
+;  
+;  (def htmlTop "
+;  <html>
+;  <body>
+;  <table border=1 width=100%>
+;  <tr>
+;  <td nowrap></td>
+;  <th width=16%>Monday</th>
+;  <th width=16%>Tuesday</th>
+;  <th width=16%>Wednesday</th>
+;  <th width=16%>Thursday</th>
+;  <th width=16%>Friday</th>
+;  </tr>
+;  ")
+;  
+;  
+;  
+;  (def html (str htmlTop htmlRows 
+;  "</table>
+;  </body>
+;  </html>"))
+;  
+;  (println "Done")
+;  
+;  
+;  (spit "../student_schedule.html" html)
+;  
+;  
