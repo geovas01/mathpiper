4d68e7874784259ed0db785bdf6ffb74336d0aa3
jakosan <jakosan@613135f6-e55d-11de-9043-17b7bd24f792>
2012-05-30 18:55:15 +0000

    Added some error handling ability to find-schedules. Added a function that will send a string with all the course names separated by commas called formated-courses-in-map.

--- experimental/student_schedule/student_schedule/src/org/mathpiper/studentschedule/completescheduleengine/student_schedules_api.clj
+++ experimental/student_schedule/student_schedule/src/org/mathpiper/studentschedule/completescheduleengine/student_schedules_api.clj
@@ -16,9 +16,14 @@
   (let [
         {course-lists :course-lists quality-fn-and-vals :quality-fn-and-vals return-number :return-number custom-courses :custom-courses} (load-string string)
         custom-course-map (merge zz2 custom-courses)
+        all-courses (apply concat course-lists)
+        courses-not-in-map (filter (fn [course] (not (course  zz2))) all-courses)
         legal-schedules-output  (legal-schedules course-lists  custom-course-map)
         ]
-    (cond (and (not= quality-fn-and-vals []))
+    (cond (not= courses-not-in-map '())
+            (apply str (concat ["The following course numbers either are not offered this semester or do not exist \n"] (map #(str "   " (name %) " \n " ) courses-not-in-map) ) )
+            
+            (and (not= quality-fn-and-vals []))
      (createHtmlScheduleTables (take return-number  (sort-by-quality legal-schedules-output quality-fn-and-vals 0.0 custom-course-map))
        custom-course-map)
      
@@ -29,11 +34,18 @@
     )
   )
 
+(defn  formated-courses-in-map []
+  (apply str (map #(str (name %) ",") (keys zz2)))
+  
+  )
+
+(formated-courses-in-map)
+
  ; test string:
 
  ;
-#_(def ali "{:course-lists [[:ETCO1120] [:ETEM1110] [:MATH1010] [:ENGL1101] [:ARTH1101 :ENGL2275 :MUSI1201 :MUSI2211 :PHIL3300 :THAR1000]]
-  :return-number 6 :quality-fn-and-vals [] #_[[time-of-day-ratio-corrected [:morning 1]]] :custom-courses {}}")
+#_(def ali "{:course-lists [[:ETCO1120] [:ETEM1110] [:MATH1300] [:ENGL1101] [:ARTH1101 :ENGL2275 :MUSI1201 :MUSI2211 :PHIL3300 :THAR1000]]
+  :return-number 6 :quality-fn-and-vals [[time-of-day-ratio-corrected [:afternoon 1]]] :custom-courses {}}")
 
  
  
