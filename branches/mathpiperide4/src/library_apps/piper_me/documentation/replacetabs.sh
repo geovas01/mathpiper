f9152a4387f4b1c6e329fbde7fa9ab27cafe82df
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2012-08-31 21:40:37 +0000

    This code is no longer being used.

--- branches/mathpiperide4/src/library_apps/piper_me/documentation/replacetabs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-perl -e '($_ = join "",<>) =~ s/(\t)/  /g; print;' $1
--- branches/mathpiperide4/src/library_apps/piper_me/piper_me/org/mathrider/piper_me/LispGlobal.java
+++
@@ -1,7 +0,0 @@
-package org.mathpiper.ide.piper_me;
-
-
-class LispGlobal extends LispAssociatedHash // <LispGlobalVariable>
-{
-}
-
