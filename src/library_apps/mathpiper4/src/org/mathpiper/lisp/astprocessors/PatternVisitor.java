0d3432e17cdede8a2d116658a0382d72d4f79833
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2013-03-17 07:43:31 +0000

    Added support for matching all nodes in a tree using a single pattern variable. Added support for matching subtrees.

--- src/library_apps/mathpiper4/src/org/mathpiper/lisp/astprocessors/PatternVisitor.java
+++ src/library_apps/mathpiper4/src/org/mathpiper/lisp/astprocessors/PatternVisitor.java
@@ -12,9 +12,7 @@ import org.mathpiper.lisp.cons.Cons;
 import org.mathpiper.lisp.cons.SublistCons;
 import org.mathpiper.lisp.parametermatchers.ParametersPatternMatcher;
 
-
-public class PatternVisitor
-        implements ASTProcessor {
+public class PatternVisitor implements ASTProcessor {
 
     Environment iEnvironment;
     ParametersPatternMatcher matcher = null;
@@ -22,112 +20,139 @@ public class PatternVisitor
     String operatorString;
 
 
+
     public PatternVisitor(Environment aEnvironment, Cons pattern, Cons associationList) throws Throwable {
-        iEnvironment = aEnvironment;
-        
-        
-        //check that associationList is a compound object
-        if(! (associationList.car() instanceof Cons)) LispError.checkArgument(aEnvironment, -1, 2);
-        Cons listCons = (Cons) associationList.car();
-        if( listCons == null) LispError.checkArgument(aEnvironment, -1, 2);
-        listCons = listCons.cdr();
-        this.associationList = listCons;
-        
-        Cons postPredicate = Utility.getTrueAtom(aEnvironment);
-        
-        if(pattern.car() instanceof Cons)
-        {
-            Cons operator = (Cons) pattern.car();
-            
-            operatorString = (String) operator.car(); 
-            
-            if(! operatorString.equals("_"))
-            {
-                pattern = operator.cdr();
-                
-                matcher = new org.mathpiper.lisp.parametermatchers.ParametersPatternMatcher(aEnvironment, -1, pattern, postPredicate);
-            }
-            
-            
-        //PatternContainer patternContainer = new PatternContainer(matcher);
-        }
-        else
-        {
-            operatorString = (String) pattern.car();
-        }
-        
-    }
+	iEnvironment = aEnvironment;
 
+	//check that associationList is a compound object
+	if (!(associationList.car() instanceof Cons))
+	    LispError.checkArgument(aEnvironment, -1, 2);
+	if (associationList.car() == null)
+	    LispError.checkArgument(aEnvironment, -1, 2);
 
-    public Cons matches(Environment aEnvironment, int aStackTop, Cons aElement)
-            throws Throwable {
+	this.associationList = associationList;
 
-	
-	Object nodeSymbol =  aElement.car();
-		
+	Cons postPredicate = Utility.getTrueAtom(aEnvironment);
 
-	if (operatorString.equals(nodeSymbol) || operatorString.equals("_")) {
-	    
-	    if(matcher == null)
-	    {
-		//Obtain the function from the association list;
-		Cons result = Utility.associativeListGet(aEnvironment, aStackTop, AtomCons.getInstance(aEnvironment, aStackTop, "\"function\""), associationList);
-		if(result != null)
-		{
-		    result = ((Cons) result.car()).cdr().cdr();
+	if (pattern.car() instanceof Cons) {
+	    Cons operator = (Cons) pattern.car();
+
+	    operatorString = (String) operator.car();
+
+	    if (!(operatorString.equals("_") && operator.cdr().cdr() == null)) {
+
+		if (!operatorString.equals("_")) {
+		    pattern = operator.cdr();
 		}
-		Cons function = result;
-		
-		//Cons listAtom1 = AtomCons.getInstance(aEnvironment, aStackTop, "List");
-		
-		//listAtom1.setCdr(associationList);
-		
-		//Cons associationSubList = SublistCons.getInstance(aEnvironment, listAtom1);
-		
-		Cons listAtom = AtomCons.getInstance(aEnvironment, aStackTop, "List");
-		
-		listAtom.setCdr(associationList);
-		
-		Cons sublist = SublistCons.getInstance(aEnvironment, listAtom);
-		
-		Cons storeCdr = aElement.cdr();
-		
-		aElement.setCdr(null); //Done so that Utility.applyPure does not throw an exception.
-		
-		sublist.setCdr(aElement);
-		
-		result = Utility.applyPure(aStackTop, function, sublist, aEnvironment);
-		
-	        aElement.setCdr(storeCdr);
-	        
-	        Map metaMap = result.getMetadataMap();
-	        
-	        aElement.setMetadataMap(metaMap);
-		
-		System.out.println("Match: " + aElement.toString());
-		return null;
-	    }
-	    
-	    if (matcher.matches(aEnvironment, aStackTop, aElement.cdr())) {
-		System.out.println("Match: " + aElement.toString());
-		return null;
+
+		matcher = new org.mathpiper.lisp.parametermatchers.ParametersPatternMatcher(aEnvironment, -1, pattern,
+			postPredicate);
 	    }
 
+	} else {
+	    operatorString = (String) pattern.car();
 	}
 
-        return null;
     }
-    
-    
-    public Cons getAssociationList(Environment aEnvironment, int aStackTop) throws Throwable
-    {
-	Cons listAtom = AtomCons.getInstance(aEnvironment, aStackTop, "List");
 
-	listAtom.setCdr(associationList);
 
-	Cons sublist = SublistCons.getInstance(aEnvironment, listAtom);
 
-	return sublist;
+    public Cons matches(Environment aEnvironment, int aStackTop, Cons aElement) throws Throwable {
+
+	Object nodeSymbol;
+
+	if (aElement instanceof SublistCons) {
+	    nodeSymbol = Cons.caar(aElement);
+
+	    if (operatorString.equals(nodeSymbol) && matcher != null) {
+
+		//aElement.setCdr(null);
+
+		if (matcher.matches(aEnvironment, aStackTop, (Cons) Cons.cdar(aElement))) {
+		    //Obtain the function from the association list;
+		    Cons result = Utility.associativeListGet(aEnvironment, aStackTop,
+			    AtomCons.getInstance(aEnvironment, aStackTop, "\"function\""), ((Cons) associationList.car()).cdr());
+		    if (result != null) {
+			result = ((Cons) result.car()).cdr().cdr();
+		    }
+		    Cons function = result;
+
+		    associationList.setCdr(aElement);
+
+		    result = Utility.applyPure(aStackTop, function, associationList, aEnvironment);
+
+		    associationList.setCdr(null);
+
+		    Map metaMap = result.getMetadataMap();
+
+		    aElement.setMetadataMap(metaMap);
+
+		    System.out.println("Match: " + aElement.toString());
+		    return null;
+		}
+	    }
+	} else {
+	    nodeSymbol = aElement.car();
+	    if (operatorString.equals(nodeSymbol) || operatorString.equals("_")) {
+
+		if (matcher == null) {
+		    //Obtain the function from the association list;
+		    Cons result = Utility.associativeListGet(aEnvironment, aStackTop,
+			    AtomCons.getInstance(aEnvironment, aStackTop, "\"function\""), ((Cons) associationList.car()).cdr());
+		    if (result != null) {
+			result = ((Cons) result.car()).cdr().cdr();
+		    }
+		    Cons function = result;
+
+		    associationList.setCdr(aElement);
+
+		    result = Utility.applyPure(aStackTop, function, associationList, aEnvironment);
+
+		    associationList.setCdr(null);
+
+		    Map metaMap = result.getMetadataMap();
+
+		    aElement.setMetadataMap(metaMap);
+
+		    System.out.println("Match: " + aElement.toString());
+		    return null;
+		} else {
+		    if (matcher.matches(aEnvironment, aStackTop, aElement)) {
+			//Obtain the function from the association list;
+			Cons result = Utility
+				.associativeListGet(aEnvironment, aStackTop,
+					AtomCons.getInstance(aEnvironment, aStackTop, "\"function\""),
+					((Cons) associationList.car()).cdr());
+			if (result != null) {
+			    result = ((Cons) result.car()).cdr().cdr();
+			}
+			Cons function = result;
+
+			associationList.setCdr(aElement);
+
+			result = Utility.applyPure(aStackTop, function, associationList, aEnvironment);
+
+			associationList.setCdr(null);
+
+			Map metaMap = result.getMetadataMap();
+
+			aElement.setMetadataMap(metaMap);
+
+			System.out.println("Match: " + aElement.toString());
+			return null;
+		    }
+		}
+
+	    }//end if.
+	}
+
+	return null;
+    }
+
+
+
+    public Cons getAssociationList(Environment aEnvironment, int aStackTop) throws Throwable {
+	return associationList;
     }
 
 };
