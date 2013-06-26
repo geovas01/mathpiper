83e35c077111fcc6c3f22546309ef4226b909420
ted.kosan <ted.kosan@613135f6-e55d-11de-9043-17b7bd24f792>
2013-06-26 07:11:23 +0000

    Reworked the .java file generating code so that generated .java files are placed into the build directory.

--- branches/mathpiperide4/src/library_apps/mathpiper4/src/org/mathpiper/Version.java
+++ branches/mathpiperide4/src/library_apps/mathpiper4/src/org/mathpiper/Version.java
@@ -4,7 +4,7 @@ package org.mathpiper;
 
 public class Version
 {
-   private static final String version = "mp4_minimal_alpha_v.114";
+   private static final String version = "";
 
    public static String version()
    {
